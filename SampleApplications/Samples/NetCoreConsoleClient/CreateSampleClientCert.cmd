b749ef253050dac3c4a8bcbb20394e9220689d5c
Martin Regen <mregen@microsoft.com>
2016-07-25 22:13:19 +0200

    Console client cert fixes: remove default certs, add windows batch to create app certs

---
+++ SampleApplications/Samples/NetCoreConsoleClient/CreateSampleClientCert.cmd
@@ -0,0 +1,8 @@
+REM create the app certificate for NetCoreConsoleClient
+set CERTSTORE=".\OPC Foundation\CertificateStores\MachineDefault"
+rd /S/Q %CERTSTORE%
+md %CERTSTORE%
+Opc.Ua.CertificateGenerator.exe -cmd issue -sp %CERTSTORE% -an "UA Sample Client" -dn %COMPUTERNAME% -sn "CN=UA Sample Client/DC=%COMPUTERNAME%" -au "urn:localhost:OPCFoundation:SampleClient
+set CERTSTORE=
+
+
