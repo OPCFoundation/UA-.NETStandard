cf8084fe3a0d680f003ae732c11139f04ae72d7c
Martin Regen <mregen@martins-mac.fritz.box>
2016-07-27 07:27:24 +0200

    Parse HOSTNAME for . on Linux platforms returning a FQDN (MacOS)

--- SampleApplications/Samples/NetCoreConsoleClient/createcert.sh
+++ SampleApplications/Samples/NetCoreConsoleClient/createcert.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Create a UA Sample Client cert for OPC UA client
 #
-openssl req -newkey rsa:1024 -out cert.crt -keyout cert.key -subj "/DC=$HOSTNAME/CN=UA Sample Client" -nodes
+MYHOSTNAME=`echo $HOSTNAME | cut -f1 -d. `
+echo Create certificate $MYHOSTNAME for UA Sample Client
+openssl req -newkey rsa:1024 -out cert.crt -keyout cert.key -subj "/DC=$MYHOSTNAME/CN=UA Sample Client" -nodes
 openssl x509 -days 365 -req -in cert.crt -signkey cert.key -out cert.pem -sha1 -extfile sampleclient.ext 
-openssl pkcs12 -export -in cert.pem -inkey cert.key -out $HOSTNAME.pfx -nodes 
-openssl x509 -in cert.pem -out $HOSTNAME.der -outform DER 
-openssl x509 -in $HOSTNAME.der -inform DER -text
+openssl pkcs12 -export -in cert.pem -inkey cert.key -out $MYHOSTNAME.pfx -nodes 
+openssl x509 -in cert.pem -out $MYHOSTNAME.der -outform DER 
+openssl x509 -in $MYHOSTNAME.der -inform DER -text
 rm cert.*
-mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/certs"
-mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/private"
-mv $HOSTNAME.der "./OPC Foundation/CertificateStores/MachineDefault/certs"
-mv $HOSTNAME.pfx "./OPC Foundation/CertificateStores/MachineDefault/private"
+mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/certs/*"
+mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/private/*"
+mv $MYHOSTNAME.der "./OPC Foundation/CertificateStores/MachineDefault/certs"
+mv $MYHOSTNAME.pfx "./OPC Foundation/CertificateStores/MachineDefault/private"
 
--- SampleApplications/Samples/NetCoreConsoleServer/createcert.sh
+++ SampleApplications/Samples/NetCoreConsoleServer/createcert.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Create a UA Sample Server cert for OPC UA server
 #
-openssl req -newkey rsa:1024 -out cert.crt -keyout cert.key -subj "/DC=$HOSTNAME/CN=UA Sample Server" -nodes
+MYHOSTNAME=`echo $HOSTNAME | cut -f1 -d. `
+echo Create certificate $MYHOSTNAME for UA Sample Server
+openssl req -newkey rsa:1024 -out cert.crt -keyout cert.key -subj "/DC=$MYHOSTNAME/CN=UA Sample Server" -nodes
 openssl x509 -days 365 -req -in cert.crt -signkey cert.key -out cert.pem -sha1 -extfile sampleserver.ext 
-openssl pkcs12 -export -in cert.pem -inkey cert.key -out $HOSTNAME.pfx -nodes 
-openssl x509 -in cert.pem -out $HOSTNAME.der -outform DER 
-openssl x509 -in $HOSTNAME.der -inform DER -text
+openssl pkcs12 -export -in cert.pem -inkey cert.key -out $MYHOSTNAME.pfx -nodes 
+openssl x509 -in cert.pem -out $MYHOSTNAME.der -outform DER 
+openssl x509 -in $MYHOSTNAME.der -inform DER -text
 rm cert.*
 mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/certs"
 mkdir -p "./OPC Foundation/CertificateStores/MachineDefault/private"
-mv $HOSTNAME.der "./OPC Foundation/CertificateStores/MachineDefault/certs"
-mv $HOSTNAME.pfx "./OPC Foundation/CertificateStores/MachineDefault/private"
+mv $MYHOSTNAME.der "./OPC Foundation/CertificateStores/MachineDefault/certs"
+mv $MYHOSTNAME.pfx "./OPC Foundation/CertificateStores/MachineDefault/private"
 
