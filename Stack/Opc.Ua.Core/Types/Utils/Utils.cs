352cdcf861b5409d4a118a50cae403d44ca99c87
Martin Regen <mregen@microsoft.com>
2017-08-07 20:08:49 +0200

    fix for CTT manual test failure: Arrays2D Failed with DataItem/ExpandedNodeId: Reason array2D fails to clone Implement Array Clone for any dimensional array, fix random generator which may produce array length 0

--- Stack/Opc.Ua.Core/Types/Utils/Utils.cs
+++ Stack/Opc.Ua.Core/Types/Utils/Utils.cs
@@ -1389,18 +1389,46 @@ namespace Opc.Ua
                 return value;
             }
 
-            // copy arrays.
+            // copy arrays, any dimension.
             Array array = value as Array;
             if (array != null)
             {
-                Array clone = Array.CreateInstance(type.GetElementType(), array.Length);
-
-                for (int ii = 0; ii < array.Length; ii++)
+                if (array.Rank == 1)
                 {
-                    clone.SetValue(Utils.Clone(array.GetValue(ii)), ii);
+                    Array clone = Array.CreateInstance(type.GetElementType(), array.Length);
+                    for (int ii = 0; ii < array.Length; ii++)
+                    {
+                        clone.SetValue(Utils.Clone(array.GetValue(ii)), ii);
+                    }
+                    return clone;
                 }
+                else
+                {
+                    int[] arrayRanks = new int[array.Rank];
+                    int[] arrayIndex = new int[array.Rank];
+                    for (int ii=0; ii<array.Rank; ii++)
+                    {
+                        arrayRanks[ii] = array.GetLength(ii);
+                        arrayIndex[ii] = 0;
+                    }
+                    Array clone = Array.CreateInstance(type.GetElementType(), arrayRanks);
+                    for (int ii = 0; ii < array.Length; ii++)
+                    {
+                        clone.SetValue(Utils.Clone(array.GetValue(arrayIndex)), arrayIndex);
 
-                return clone;
+                        // iterate the index array
+                        for (int ix = 0; ix < array.Rank; ix++)
+                        {
+                            arrayIndex[ix]++;
+                            if (arrayIndex[ix] < arrayRanks[ix])
+                            {
+                                break;
+                            }
+                            arrayIndex[ix] = 0;
+                        }
+                    }
+                    return clone;
+                }
             }
 
             // copy XmlNode.
