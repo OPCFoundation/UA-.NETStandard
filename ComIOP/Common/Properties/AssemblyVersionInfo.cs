27ff681320880ecb59e0c78418e160d0e09751ff
AlinMoldovean <aam@softing.com>
2018-03-29 16:23:01 +0300

    Assembly version set to 1.03.352

--- ComIOP/Common/Properties/AssemblyVersionInfo.cs
+++ ComIOP/Common/Properties/AssemblyVersionInfo.cs
@@ -38,10 +38,10 @@ internal static class AssemblyVersionInfo
     /// <summary>
     /// The current build version.
     /// </summary>
-    public const string CurrentVersion = "1.03.351";
+    public const string CurrentVersion = "1.03.352";
 
     /// <summary>
     /// The current build file version.
     /// </summary>
-    public const string CurrentFileVersion = "1.03.351.0";
+    public const string CurrentFileVersion = "1.03.352.0";
 }
