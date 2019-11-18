2d950fef2db6920463e63378c664613019135b88
Randy Armstrong <randy@sparhawksoftware.com>
2019-11-17 22:00:16 -0800

    Remove all files except the ones needs for PWN2OWN event.

--- ComIOP/Common/Common/BuildDesign.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-SET PATH=%PATH%;..\..\..\Scripts;..\..\..\Bin;..\..\..\..\Bin
-
-Opc.Ua.ModelDesigner.exe -d2 ".\ModelDesign.xml" -c ".\ModelDesign.csv" -o ".\" 
--- SampleApplications/Samples/GDS/ConsoleServer/dockerbuild.bat
+++
@@ -1,3 +0,0 @@
-dotnet build NetCoreGlobalDiscoveryServer.csproj
-dotnet publish NetCoreGlobalDiscoveryServer.csproj -o ./publish
-docker build -t gds .
