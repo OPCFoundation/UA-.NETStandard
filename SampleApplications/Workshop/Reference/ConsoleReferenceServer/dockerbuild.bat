9d050945b2079a44384f658d4c26c613cad3f1ba
Randy Armstrong <randy.armstrong@unifiedautomation.com>
2019-12-10 23:54:28 -0700

    Remove ConsoleReferenceServer which was left in by mistake.

--- SampleApplications/Workshop/Reference/ConsoleReferenceServer/dockerbuild.bat
+++
@@ -1,4 +0,0 @@
-REM build a docker container of the console reference server
-dotnet build ConsoleReferenceServer.csproj
-dotnet publish ConsoleReferenceServer.csproj -o ./publish
-docker build -t consolerefserver .
--- SampleApplications/Workshop/Reference/ConsoleReferenceServer/dockerrun.bat
+++
@@ -1,3 +0,0 @@
-echo Run a docker container of the console reference server
-echo The certificate store of the ref server is mapped to './OPC Foundation'
-docker run -it -p 62541:62541 -h refserver -v "%CD%/OPC Foundation:/root/.local/share/OPC Foundation" consolerefserver:latest
