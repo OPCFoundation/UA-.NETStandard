710b053840a362acfce57e0fffb48167fe62e026
Martin Regen <mregen@microsoft.com>
2020-01-17 15:06:10 +0100

    fix namespace issue and switch tests to silent mode

--- SampleApplications/Samples/GDS/ClientTest/GlobalDiscoveryTestClient.cs
+++ SampleApplications/Samples/GDS/ClientTest/GlobalDiscoveryTestClient.cs
@@ -1,4 +1,4 @@
-﻿/* ========================================================================
+/* ========================================================================
  * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
  *
  * OPC Foundation MIT License 1.00
@@ -64,7 +64,7 @@ namespace Opc.Ua.Gds.Test
             Config = await application.LoadApplicationConfiguration(false);
 
             // check the application certificate.
-            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, 0);
+            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(true, 0);
             if (!haveAppCertificate)
             {
                 throw new Exception("Application instance certificate invalid!");
