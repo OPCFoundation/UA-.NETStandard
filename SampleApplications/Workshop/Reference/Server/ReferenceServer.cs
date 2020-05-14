7ce486f9fe4ea40026fdd416139da67780d47f03
AlinMoldovean <aam@softing.com>
2020-05-14 17:19:02 +0300

    ReferenceServer set V1.04 in ProductUri

--- SampleApplications/Workshop/Reference/Server/ReferenceServer.cs
+++ SampleApplications/Workshop/Reference/Server/ReferenceServer.cs
@@ -83,12 +83,10 @@ namespace Quickstarts.ReferenceServer
 
             properties.ManufacturerName = "OPC Foundation";
             properties.ProductName = "Quickstart Reference Server";
-            properties.ProductUri = "http://opcfoundation.org/Quickstart/ReferenceServer/v1.03";
+            properties.ProductUri = "http://opcfoundation.org/Quickstart/ReferenceServer/v1.04";
             properties.SoftwareVersion = Utils.GetAssemblySoftwareVersion();
             properties.BuildNumber = Utils.GetAssemblyBuildNumber();
             properties.BuildDate = Utils.GetAssemblyTimestamp();
-
-            // TBD - All applications have software certificates that need to added to the properties.
 
             return properties;
         }
@@ -201,7 +199,7 @@ namespace Quickstarts.ReferenceServer
 
                 if (args.Identity is SystemConfigurationIdentity)
                 {
-                    // set ConfigureAdmin and  role for user with permission to configure server
+                    // set ConfigureAdmin role for user with permission to configure server
                     args.Identity.GrantedRoleIds.Add(ObjectIds.WellKnownRole_ConfigureAdmin);
                     args.Identity.GrantedRoleIds.Add(ObjectIds.WellKnownRole_SecurityAdmin);
                 }
