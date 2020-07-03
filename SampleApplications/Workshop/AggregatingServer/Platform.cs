2acb8c9ee614b0cf2742572fcc56a8e33c79d182
Rudolf Pribis <rudolf.pribis@stuba.sk>
2020-07-03 16:41:34 +0200

    commit

---
+++ SampleApplications/Workshop/AggregatingServer/Platform.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.Ua;
+using System.Timers;
+using AggregatingServer.Clients;
+using AggregatingServer.Servers;
+
+namespace AggregatingServer.Core
+{
+
+    /// <summary>Class <c>AggregatingServer</c> represents Aggregating Server.</summary>
+    public class Platform
+    {
+        #region Properties
+
+        List<DiscoveryClient>  discoveryClients;
+        public string urlLDS { get; set; }
+        public DateTime lastCounterResetTime { get; set; }
+        public ServerOnNetworkCollectionEx serversOnNetwork { get; set; }
+        public int timerFindServerPeriod { get; set; }
+        protected Timer timerFindServersOnNetwork;
+        protected System.Uri uriLDS;
+        protected List<InterfaceClient> interfaceClients;
+        protected AggregatingServer.Servers.AggregatingServer aggregatingServer;
+
+
+        #endregion
+        /// <summary>
+        /// Connects to OPC UA server to create a Client Interface.
+        /// </summary>
+        /// <param name="serverID"></param>
+        /// <param name="configPath"></param>
+        public void ConnectToServer(uint serverID, string configPath)
+        {
+            ServerOnNetworkEx serverOnNetworkEx = serversOnNetwork.Find(s => s.RecordId == serverID);
+            if (serverOnNetworkEx.isConnected) return;
+            
+            InterfaceClient client = new InterfaceClient(serverID);
+
+            client.Initialize(endPointUrl: serverOnNetworkEx.DiscoveryUrl, applicationName: "DefaultClient");
+            client.LoadConfiguration(configDir: configPath, configSectionName: "DefaultClient");
+            client.Run().GetAwaiter().GetResult();
+
+            interfaceClients.Add(client);
+
+            serverOnNetworkEx.isConnected = true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Platform()
+        {
+            // set default parameters 
+            timerFindServerPeriod = 1000; // 1000 ms
+            urlLDS = "opc.tcp://localhost:4840/";
+            discoveryClients = new List<DiscoveryClient>();
+
+            interfaceClients = new List<InterfaceClient>();
+
+            // create instance 
+            aggregatingServer = new Servers.AggregatingServer();
+
+        }
+
+        /// <summary>
+        ///  UpdateTimer
+        /// </summary>
+        /// <param name="timeOut"></param>
+        public void StartFindServersOnNetwork()
+        {
+
+            uriLDS = new Uri(urlLDS);
+            discoveryClients.Add(DiscoveryClient.Create(uriLDS));
+
+
+            // Create a timer with a two second interval.
+            timerFindServersOnNetwork = new System.Timers.Timer(timerFindServerPeriod);
+            // Hook up the Elapsed event for the timer. 
+            timerFindServersOnNetwork.Elapsed += (sender, e) => OnUpdateTimer(sender,e,this);
+            timerFindServersOnNetwork.AutoReset = true;
+            timerFindServersOnNetwork.Enabled = false;
+
+            FindServersOnNetwork(this);
+
+        }
+
+        /// <summary>
+        ///  OnUpdateTimer
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        /// <param name="obj"></param>
+        private static void OnUpdateTimer(Object source, ElapsedEventArgs e, Object obj)
+        {
+            Platform aggregatingServer = (Platform)obj;
+            //FindServersOnNetwork("opc.tcp://VM-WIN16-DEV:4840/");
+            FindServersOnNetwork(aggregatingServer);
+        }
+
+        #region Methods
+        /// <summary>
+        /// FindServersOnNetwork
+        /// </summary>
+        /// <param name="aggregatingServer"></param>
+        static public void FindServersOnNetwork(Platform aggregatingServer)
+        {
+            DateTime lastCounterResetTime;
+            ServerOnNetworkCollection serverOnNetworks;
+
+            try
+            {
+                foreach (DiscoveryClient discoveryClient in aggregatingServer.discoveryClients)
+                {
+                    discoveryClient.FindServersOnNetwork(null, 0, 0, null, out lastCounterResetTime, out serverOnNetworks);
+
+                    aggregatingServer.lastCounterResetTime = lastCounterResetTime;
+                    aggregatingServer.serversOnNetwork = (ServerOnNetworkCollectionEx)serverOnNetworks;
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+        /// <summary>
+        /// Start aggregating server
+        /// </summary>
+        public void AggregatingServerStart()
+        {
+            aggregatingServer.Initialize(applicationName: "phi-ware Aggregating Server");
+            aggregatingServer.LoadConfiguration(configSectionName: "AggregatingServer");
+            aggregatingServer.CheckCertificate();
+            aggregatingServer.Run().GetAwaiter().GetResult();
+
+            // Post-creation actions
+            aggregatingServer.PostCreate();
+
+
+            Console.WriteLine("aggregating server started: " + Environment.NewLine + string.Join(Environment.NewLine, aggregatingServer.baseAddresses) );
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serverID"></param>
+        public void AggregatingServerAggregate(uint serverID)
+        {
+
+            // find the client which refers to serverID
+            InterfaceClient interfaceClient;
+            
+            interfaceClient = interfaceClients.Find(i => i.serverOnNetworkID == serverID);
+
+            // Add namespace
+            foreach(string ns in interfaceClient.GetNamespaces())
+            {
+                // add namespace to aggregating server
+                aggregatingServer.AddNamespaceUri(ns);
+
+                /*
+
+                // browse 
+                Byte[] continuationPoint;
+                ReferenceDescriptionCollection references;
+                ResponseHeader responseHeader;
+
+                responseHeader = interfaceClient.Browse(ObjectIds.ObjectsFolder, BrowseDirection.Forward, true,
+                    (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method,
+                    ReferenceTypeIds.HierarchicalReferences, out continuationPoint, out references);
+                    */
+            }
+
+            // get server's name
+            ServerOnNetworkEx serverOnNetwork = serversOnNetwork.Find(s => s.RecordId == serverID);
+            
+            // Connect to OPC server
+
+                                    
+
+            aggregatingServer.AddNode(ObjectIdsAS.AggregatedServersFolder,
+                serverOnNetwork.ServerName,
+                serverOnNetwork.ServerName, ObjectTypeIds.BaseObjectType, 2 /* FIX HERE*/);
+
+
+        }
+
+        #endregion
+    }
+
+}
---
+++ SampleApplications/Workshop/AggregatingServer/ServerOnNetworkEx.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.Ua;
+
+namespace AggregatingServer.Core
+{
+    public class ServerOnNetworkEx : ServerOnNetwork
+    {
+        public bool isConnected { get; set; }
+        public bool isAggregated { get; set; }
+        //public uint ID { get; }
+
+        public ServerOnNetworkEx()
+        {
+
+        }
+
+        /// <summary>
+        /// Converts a collection to an array.
+        /// </summary>
+        public ServerOnNetworkEx(ref ServerOnNetwork value, uint ID)
+        {
+            
+            //this.BinaryEncodingId = value.BinaryEncodingId;
+            this.DiscoveryUrl = value.DiscoveryUrl;
+            this.isConnected = false;
+            isAggregated = false;
+            this.RecordId = value.RecordId;
+            this.ServerCapabilities = value.ServerCapabilities;
+            this.ServerName = value.ServerName;
+            //this.ID = ID;
+  
+
+
+            /*
+            Type sourceType = this.GetType();
+            Type destinationType = value.GetType();
+
+            foreach (PropertyInfo sourceProperty in sourceType.GetProperties())
+            {
+                PropertyInfo destinationProperty = destinationType.GetProperty(sourceProperty.Name);
+                if (destinationProperty != null && destinationProperty.CanWrite)
+                {
+                    destinationProperty.SetValue(value, sourceProperty.GetValue(this, null), null);
+                }
+
+
+            }
+            */
+        }
+
+    }
+
+
+    public partial class ServerOnNetworkCollectionEx : List<ServerOnNetworkEx>
+
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes the collection with default values.
+        /// </summary>
+        public ServerOnNetworkCollectionEx() { }
+
+        /// <summary>
+        /// Initializes the collection with an initial capacity.
+        /// </summary>
+        public ServerOnNetworkCollectionEx(int capacity) : base(capacity) { }
+
+        /// <summary>
+        /// Initializes the collection with another collection.
+        /// </summary>
+        public ServerOnNetworkCollectionEx(IEnumerable<ServerOnNetworkEx> collection) : base(collection) { }
+        #endregion
+
+        #region Static Operators
+        /// <summary>
+        /// Converts an array to a collection.
+        /// </summary>
+        public static implicit operator ServerOnNetworkCollectionEx(ServerOnNetworkEx[] values)
+        {
+            if (values != null)
+            {
+                return new ServerOnNetworkCollectionEx(values);
+            }
+
+            return new ServerOnNetworkCollectionEx();
+        }
+
+        /// <summary>
+        /// Converts a collection to an array.
+        /// </summary>
+        public static explicit operator ServerOnNetworkEx[] (ServerOnNetworkCollectionEx values)
+        {
+            if (values != null)
+            {
+                return values.ToArray();
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Converts a collection to an array.
+        /// </summary>
+        public static explicit operator ServerOnNetworkCollectionEx(ServerOnNetworkCollection serverOnNetworksCollection)
+        {
+            uint ID= 0;
+            ServerOnNetworkCollectionEx myCollection = new ServerOnNetworkCollectionEx();
+            foreach(ServerOnNetwork serverOnNetwork in serverOnNetworksCollection)
+            {
+                ServerOnNetwork tmp = serverOnNetwork;
+                myCollection.Add(new  ServerOnNetworkEx(ref tmp, ++ID));
+            }
+
+            return myCollection;
+        }
+        #endregion
+
+#if !NET_STANDARD
+        #region ICloneable Methods
+        /// <summary>
+        /// Creates a deep copy of the collection.
+        /// </summary>
+        public object Clone()
+        {
+            return (ServerOnNetworkCollection)this.MemberwiseClone();
+        }
+        #endregion
+#endif
+
+        /// <summary cref="Object.MemberwiseClone" />
+        public new object MemberwiseClone()
+        {
+            ServerOnNetworkCollectionEx clone = new ServerOnNetworkCollectionEx(this.Count);
+
+            for (int ii = 0; ii < this.Count; ii++)
+            {
+                clone.Add((ServerOnNetworkEx)Utils.Clone(this[ii]));
+            }
+
+            return clone;
+        }
+    }
+}
