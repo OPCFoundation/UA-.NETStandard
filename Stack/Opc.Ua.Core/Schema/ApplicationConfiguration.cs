34d0af8dd64db01340966511244a98e7e2e2ff5b
Logan Komanetz <logan.komanetz@gmail.com>
2020-11-14 04:15:01 -0600

    Removes assigning a new instance of CertificateValidator in ApplicationConfiguration (#1161)
    * Removes assigning a new instance of CertificateValidator in ApplicationConfiguration.Validate method by assigning it on construction.  
    * Refactors the code to use 'using' statements for streams and LINQ expressions.
    * Makes the CertificateValidator member readonly
    * Removes read-only status of CertificateValidator and adds unit test to ensure it is assignable from outside the class.

--- Stack/Opc.Ua.Core/Schema/ApplicationConfiguration.cs
+++ Stack/Opc.Ua.Core/Schema/ApplicationConfiguration.cs
@@ -44,6 +44,7 @@ namespace Opc.Ua
             m_transportConfigurations = new TransportConfigurationCollection();
             m_disableHiResClock = true;
             m_properties = new Dictionary<string, object>();
+            m_certificateValidator = new CertificateValidator();
         }
 
         /// <summary>
