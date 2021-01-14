01a4aa088f7385dc81074eeaa9fb9ff40b056829
Alin Moldovean <aam@softing.com>
2021-01-14 14:23:18 +0200

    Fix NuGet build

--- Libraries/Opc.Ua.PubSub/Properties/AssemblyInfo.cs
+++ Libraries/Opc.Ua.PubSub/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@
 using System.Runtime.CompilerServices;
 
 #if SIGNASSEMBLY
-[assembly: InternalsVisibleTo("Opc.Ua.PubSub.Tests, PublicKey = " +
+[assembly: InternalsVisibleTo("Opc.Ua.PubSub.Tests.Disabled, PublicKey = " +
     // OPC Foundation Strong Name Public Key
     "0024000004800000940000000602000000240000525341310004000001000100d987b12f068b35" +
     "80429f3dde01397508880fc7e62621397618456ca1549aeacfbdb90c62adfe918f05ce3677b390" +
@@ -38,5 +38,5 @@ using System.Runtime.CompilerServices;
     "ec5ae574128fc9342cb57cb8285aa4e5b589c0ebef3be571b5c8f2ab1067f7c880e8f8882a73c8" +
     "0a12a1ef")]
 #else
-[assembly: InternalsVisibleTo("Opc.Ua.PubSub.Tests")]
+[assembly: InternalsVisibleTo("Opc.Ua.PubSub.Tests.Disabled")]
 #endif
