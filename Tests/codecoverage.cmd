270fcebfe199f1196e7401eeebce1b74dd713031
Martin Regen <mregen@microsoft.com>
2021-04-13 13:02:31 +0200

    codecov update

--- Tests/codecoverage.cmd
+++ Tests/codecoverage.cmd
@@ -11,12 +11,14 @@ set build_root=%current-path%\..
 
 cd %build_root%
 
-dotnet test "UA Core Library.sln" -v n --configuration Release --collect:"XPlat Code Coverage"
+rd /s /Q .\CodeCoverage
+rd /s /Q .\TestResults
+dotnet test "UA Core Library.sln" -v n --configuration Release --collect:"XPlat Code Coverage" --results-directory ./TestResults
 
 REM ensure latest report tool is installed
 dotnet tool uninstall -g dotnet-reportgenerator-globaltool
 dotnet tool install -g dotnet-reportgenerator-globaltool
-reportgenerator -reports:./**/coverage.cobertura.xml -targetdir:./CodeCoverage -reporttypes:Badges;Html;HtmlSummary;Cobertura
+reportgenerator -reports:./TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage -reporttypes:Badges;Html;HtmlSummary;Cobertura
 
 REM Display result in browser
 .\CodeCoverage\index.html
