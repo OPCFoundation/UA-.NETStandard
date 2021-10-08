944a3e91b13bbbf8c7f82b07674703e91b8c8acb
Martin Regen <mregen@microsoft.com>
2021-10-08 17:35:01 +0200

    Update UANodeSetHelpers.cs

--- Stack/Opc.Ua.Core/Schema/UANodeSetHelpers.cs
+++ Stack/Opc.Ua.Core/Schema/UANodeSetHelpers.cs
@@ -9,7 +9,7 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
-
+ 
 using System;
 using System.Collections.Generic;
 using System.IO;
