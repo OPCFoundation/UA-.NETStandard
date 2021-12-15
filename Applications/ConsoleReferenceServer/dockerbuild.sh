2a5069bf6ee3a282c93a8111feef2521471c281c
Martin Regen <mregen@microsoft.com>
2021-12-15 19:31:05 +0100

    fix uild

--- Applications/ConsoleReferenceServer/dockerbuild.sh
+++ Applications/ConsoleReferenceServer/dockerbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo build a docker container of the .NET Core reference server
-local buildoptions=--configuration Release -p:NoHttps=true --framework net6.0
+buildoptions=--configuration Release -p:NoHttps=true --framework net6.0
 dotnet build $buildoptions ConsoleReferenceServer.csproj
 dotnet publish $buildoptions ConsoleReferenceServer.csproj -o ./publish
 sudo docker build -t consolerefserver .
