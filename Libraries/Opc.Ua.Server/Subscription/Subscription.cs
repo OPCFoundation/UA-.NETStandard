5805b3e6811661d5942f706a2d0c01edeeed5985
Martin Regen <mregen@microsoft.com>
2022-01-03 16:27:33 +0100

    fixes for CTT

--- Libraries/Opc.Ua.Server/Subscription/Subscription.cs
+++ Libraries/Opc.Ua.Server/Subscription/Subscription.cs
@@ -918,15 +918,10 @@ namespace Opc.Ua.Server
             int overflowCount = messages.Count - (int)m_maxMessageCount;
             if (overflowCount > 0)
             {
-
                 Utils.LogWarning(
-                    "WARNING: QUEUE OVERFLOW. Dropping {2} Messages. Increase MaxMessageQueueSize. SubId={0}, MaxMessageQueueSize={1}",
-                    m_id,
-                    m_maxMessageCount,
-                    overflowCount);
-
+                    "WARNING: QUEUE OVERFLOW. Dropping {0} Messages. Increase MaxMessageQueueSize. SubId={1}, MaxMessageQueueSize={2}",
+                    overflowCount, m_id, m_maxMessageCount);
                 messages.RemoveRange(0, overflowCount);
-
             }
 
             // remove old messages if queue is full.
