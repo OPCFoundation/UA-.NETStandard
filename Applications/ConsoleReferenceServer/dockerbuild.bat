d89cfc96b610ff14cac2030b7978f35dce305446
Martin Regen <mregen@microsoft.com>
2022-02-08 09:15:09 +0100

    rename to Dockerfile.local

--- Applications/ConsoleReferenceServer/dockerbuild.bat
+++ Applications/ConsoleReferenceServer/dockerbuild.bat
@@ -2,4 +2,4 @@ REM build a docker container of the console reference server
 set buildoptions=--configuration Release -p:NoHttps=true --framework net6.0
 dotnet build %buildoptions% ConsoleReferenceServer.csproj
 dotnet publish %buildoptions% ConsoleReferenceServer.csproj -o ./publish
-docker build -t consolerefserver .
+docker build -f Dockerfile.local -t consolerefserver .
--- Applications/ConsoleReferenceServer/dockerrun.bat
+++ Applications/ConsoleReferenceServer/dockerrun.bat
@@ -1,3 +1,6 @@
+@echo off
 echo Run a docker container of the console reference server
-echo The certificate store of the ref server is mapped to './OPC Foundation'
-docker run -it -p 62541:62541 -h %COMPUTERNAME% -v "%CD%/OPC Foundation:/root/.local/share/OPC Foundation" consolerefserver:latest -c -a
+echo By default, the certificate store of the ref server is mapped to './OPC Foundation/pki'
+echo A log file is created at './OPC Foundation/Logs/Quickstarts.ReferenceServer.log.txt'
+echo A shadow configuration file for customization is created in './OPC Foundation/Quickstarts.ReferenceServer.Config.xml'
+docker run -it -p 62541:62541 -h %COMPUTERNAME% -v "%CD%/OPC Foundation:/root/.local/share/OPC Foundation" consolerefserver:latest -c -s
