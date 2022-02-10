df099147bb0d58637a6d5e00b8e0a93d527ba46d
Martin Regen <mregen@microsoft.com>
2022-02-10 15:40:20 +0100

    move back to previous paradigm

--- Applications/ConsoleReferenceServer/dockerbuild.sh
+++ Applications/ConsoleReferenceServer/dockerbuild.sh
@@ -3,4 +3,4 @@ echo build a docker container of the .NET Core reference server, without https s
 buildoptions="--configuration Release -p:NoHttps=true --framework net6.0"
 dotnet build $buildoptions ConsoleReferenceServer.csproj
 dotnet publish $buildoptions ConsoleReferenceServer.csproj -o ./publish
-sudo docker build -f Dockerfile.local -t consolerefserver .
+sudo docker build -f Dockerfile -t consolerefserver .
