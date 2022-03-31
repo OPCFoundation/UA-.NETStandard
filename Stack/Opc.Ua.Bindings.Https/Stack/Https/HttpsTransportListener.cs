4df1952c0eace67db78bf726271314c056433ecd
Suciu Mircea Adrian <Mircea-Adrian.Suciu@softing.com>
2022-03-31 12:51:18 +0300

    Removed extra license text

--- Stack/Opc.Ua.Bindings.Https/Stack/Https/HttpsTransportListener.cs
+++ Stack/Opc.Ua.Bindings.Https/Stack/Https/HttpsTransportListener.cs
@@ -4,10 +4,6 @@
      - GPL V2: everybody else
    RCL license terms accompanied with this source code. See http://opcfoundation.org/License/RCL/1.00/
    GNU General Public License as published by the Free Software Foundation;
-   version 2 of the License are accompanied with this source code. See http://opcfoundation.org/License/GPLv2
-   This source code is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 
 using System;
