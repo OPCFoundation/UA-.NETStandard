b8b363b42ad0cdcfa9737d624cfe9bc277675b1d
Jan Gjerseth <jan.gjerseth@hotmail.com>
2022-04-04 13:55:13 +0200

    Handle JWT and set the IssuedTokenType in the server based on the policy referenced by the PolicyId in the client supplied UserIdentityToken .

--- Libraries/Opc.Ua.Server/Session/Session.cs
+++ Libraries/Opc.Ua.Server/Session/Session.cs
@@ -1005,6 +1005,14 @@ namespace Opc.Ua.Server
                 throw ServiceResultException.Create(StatusCodes.BadIdentityTokenInvalid, "User token policy not supported.");
             }
 
+            if (token is IssuedIdentityToken issuedToken)
+            {
+                if (policy.IssuedTokenType == Profiles.JwtUserToken)
+                {
+                    issuedToken.IssuedTokenType = IssuedTokenType.JWT; 
+                }
+            }
+
             // determine the security policy uri.
             string securityPolicyUri = policy.SecurityPolicyUri;
 
