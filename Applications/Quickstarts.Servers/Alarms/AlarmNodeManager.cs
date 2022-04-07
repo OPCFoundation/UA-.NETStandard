0f3f46d7694e579d1611d6ecc2aabf05f5a5055c
DEV-2021\Archie <archie@dreamsandadventures.ca>
2022-04-07 09:04:42 -0600

    Added MIT License

--- Applications/Quickstarts.Servers/Alarms/AlarmNodeManager.cs
+++ Applications/Quickstarts.Servers/Alarms/AlarmNodeManager.cs
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Copyright (c) 2005-2020 The OPC Foundation, Inc. All rights reserved.
+ * Copyright (c) 2005-2022 The OPC Foundation, Inc. All rights reserved.
  *
  * OPC Foundation MIT License 1.00
  * 
@@ -26,6 +26,8 @@
  * The complete license agreement can be found here:
  * http://opcfoundation.org/License/MIT/1.00/
  * ======================================================================*/
+
+
 
 using System;
 using System.Collections.Generic;
