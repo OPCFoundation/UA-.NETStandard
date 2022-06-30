71028785c7321d86d5af7c95392b457945dc318d
Jan Gjerseth <jan.gjerseth@hotmail.com>
2022-06-30 09:43:58 +0200

    Added OPCF file headers

--- Tests/Opc.Ua.Client.Tests/TokenValidatorMock.cs
+++ Tests/Opc.Ua.Client.Tests/TokenValidatorMock.cs
@@ -1,13 +1,31 @@
-// This document contains confidential and proprietary information of ABB
-// and may be protected by patents, trademarks, copyrights, trade secrets,
-// and/or other relevant state, federal, and foreign laws. Its receipt or
-// possession does not convey any rights to reproduce, disclose its contents,
-// or to manufacture, use or sell anything contained herein. Forwarding,
-// reproducing, disclosing or using without specific written authorization of
-// ABB is strictly forbidden.
-//
-// See the LICENSE file in the project root for more information.
-// Copyright © ABB Ltd. All rights reserved.
+/* ========================================================================
+ * Copyright (c) 2005-2020 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
 
 using Opc.Ua;
 using Quickstarts.ReferenceServer;
