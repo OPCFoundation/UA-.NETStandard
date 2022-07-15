9b0e4431469aebdb9141edc86663b07c09b04871
Joshua Leger <jleger@ptc.com>
2022-07-15 10:24:22 -0400

    Removing unused using statement

--- Tests/Opc.Ua.Server.Tests/MasterNodeManagerTests.cs
+++ Tests/Opc.Ua.Server.Tests/MasterNodeManagerTests.cs
@@ -28,7 +28,6 @@
  * ======================================================================*/
 
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
