23341afc12fa7b0021341c18b5dc04c8c7992f0c
Martin Regen <mregen@microsoft.com>
2023-01-03 08:31:21 +0100

    fix docker version

--- Applications/ConsoleReferenceServer/dockerbuild.bat
+++ Applications/ConsoleReferenceServer/dockerbuild.bat
@@ -1,2 +1,7 @@
 REM build a docker container of the console reference server
-docker build -f %POPDIR%\Dockerfile -t consolerefserver .\..\..
+dotnet tool install --global nbgv
+for /f "delims=" %%a in ('nbgv get-version -v Version') do @set Version=%%a
+for /f "delims=" %%a in ('nbgv get-version -v SimpleVersion') do @set SimpleVersion=%%a
+for /f "delims=" %%a in ('nbgv get-version -v AssemblyInformationalVersion') do @set InformationalVersion=%%a
+echo Version Info for docker build: %Version% %SimpleVersion% %InformationalVersion%
+docker build --build-arg Version=%Version% --build-arg SimpleVersion=%SimpleVersion% --build-arg InformationalVersion=%InformationalVersion%  -f .\Dockerfile -t consolerefserver .\..\..
