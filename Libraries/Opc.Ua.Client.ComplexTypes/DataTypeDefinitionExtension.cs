65f9757b916c6f90790560036f0366cceb74bc6f
Marc Schier <marcschier@users.noreply.github.com>
2023-01-09 16:08:06 +0100

    Add an API to ComplexTypeSystem to retrieve data type definitions used during loading (#2039)
    The changes add an API to ComplexTypeSystem to retrieve data type definitions that were created during type building to leverage them to build DataSetMetaDataType for OPC UA PubSub.  
    
    The changes include the following:
    
    * Combine all conversion of enumerations into EnumDefinition and use the resulting to build the type
    * Remove all other API from the type builder interface and implementation. 
      > The assumption is that these interfaces are used internally and removal will not cause a breaking change.
    * Combine everything previously covered elsewhere into existing AddEnumType function.
    * Add a NodeIdDictionary cache and fill that cache during load, as well as API to retrieve the data type definitions

--- Libraries/Opc.Ua.Client.ComplexTypes/DataTypeDefinitionExtension.cs
+++ Libraries/Opc.Ua.Client.ComplexTypes/DataTypeDefinitionExtension.cs
@@ -2,7 +2,7 @@
  * Copyright (c) 2005-2020 The OPC Foundation, Inc. All rights reserved.
  *
  * OPC Foundation MIT License 1.00
- * 
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -11,7 +11,7 @@
  * copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following
  * conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -30,6 +30,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Xml;
 
 namespace Opc.Ua.Client.ComplexTypes
@@ -232,6 +234,80 @@ namespace Opc.Ua.Client.ComplexTypes
         }
 
         /// <summary>
+        /// Convert a binary schema enumerated type to an enum data type definition
+        /// Available before OPC UA V1.04.
+        /// </summary>
+        public static EnumDefinition ToEnumDefinition(this Schema.Binary.EnumeratedType enumeratedType)
+        {
+            var enumDefinition = new EnumDefinition();
+
+            foreach (var enumValue in enumeratedType.EnumeratedValue)
+            {
+                var enumTypeField = new EnumField
+                {
+                    Name = enumValue.Name,
+                    Value = enumValue.Value,
+                    Description = enumValue.Documentation?.Text?.FirstOrDefault(),
+                    DisplayName = enumValue.Name
+                };
+                enumDefinition.Fields.Add(enumTypeField);
+            }
+
+            return enumDefinition;
+        }
+
+        /// <summary>
+        /// Convert a list of EnumValues to an enum data type definition
+        /// Available before OPC UA V1.04.
+        /// </summary>
+        public static EnumDefinition ToEnumDefinition(this ExtensionObject[] enumValueTypes)
+        {
+            var enumDefinition = new EnumDefinition();
+
+            foreach (var extensionObject in enumValueTypes)
+            {
+                var enumValue = extensionObject.Body as EnumValueType;
+                var name = enumValue.DisplayName.Text;
+
+                var enumTypeField = new EnumField {
+                    Name = name,
+                    Value = enumValue.Value,
+                    DisplayName = name
+                };
+                enumDefinition.Fields.Add(enumTypeField);
+            }
+
+            return enumDefinition;
+        }
+
+        /// <summary>
+        /// Convert a list of EnumValues to an enum data type definition
+        /// Available before OPC UA V1.04.
+        /// </summary>
+        public static EnumDefinition ToEnumDefinition(this LocalizedText[] enumFieldNames)
+        {
+            var enumDefinition = new EnumDefinition();
+
+            for (int ii = 0; ii < enumFieldNames.Length; ii++)
+            {
+                LocalizedText enumFieldName = enumFieldNames[ii];
+                var name = enumFieldName.Text;
+
+                var enumTypeField = new EnumField {
+                    Name = name,
+                    Value = ii,
+                    DisplayName = name
+                };
+
+                enumDefinition.Fields.Add(enumTypeField);
+            }
+
+            return enumDefinition;
+        }
+        #endregion Public Extensions
+
+        #region Private Methods
+        /// <summary>
         /// Test for special Bit type used in the binary schema structure definition.
         /// </summary>
         private static bool IsXmlBitType(this XmlQualifiedName typeName)
@@ -282,6 +358,6 @@ namespace Opc.Ua.Client.ComplexTypes
                 return referenceId;
             }
         }
-        #endregion Public Extensions
+        #endregion Private Methods
     }
 }//namespace
