f7ab64b65f4bcff3d18d913820ab0ae3c03c3a3d
Martin Regen <mregen@microsoft.com>
2023-08-29 09:32:18 +0200

    typos

--- Stack/Opc.Ua.Core/Stack/State/DataTypeState.cs
+++ Stack/Opc.Ua.Core/Stack/State/DataTypeState.cs
@@ -163,12 +163,12 @@ namespace Opc.Ua
         /// </summary>
         /// <param name="context">The context.</param>
         /// <param name="decoder">The decoder.</param>
-        /// <param name="attibutesToLoad">The attributes to load.</param>
-        public override void Update(ISystemContext context, BinaryDecoder decoder, AttributesToSave attibutesToLoad)
+        /// <param name="attributesToLoad">The attributes to load.</param>
+        public override void Update(ISystemContext context, BinaryDecoder decoder, AttributesToSave attributesToLoad)
         {
-            base.Update(context, decoder, attibutesToLoad);
+            base.Update(context, decoder, attributesToLoad);
 
-            if ((attibutesToLoad & AttributesToSave.DataTypeDefinition) != 0)
+            if ((attributesToLoad & AttributesToSave.DataTypeDefinition) != 0)
             {
                 DataTypeDefinition = decoder.ReadExtensionObject(null);
             }
