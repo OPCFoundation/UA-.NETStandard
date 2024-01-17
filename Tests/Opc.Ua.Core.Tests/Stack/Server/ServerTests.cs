3a94232411c13b643a4cf821029f1f616c18e80a
Martin Regen <mregen@microsoft.com>
2024-01-17 09:20:27 +0100

    add template for a TranslateEndpointDescriptionsTest

---
+++ Tests/Opc.Ua.Core.Tests/Stack/Server/ServerTests.cs
@@ -0,0 +1,85 @@
+/* ========================================================================
+ * Copyright (c) 2005-2021 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Opc.Ua.Core.Tests.Stack.Server
+{
+    /// <summary>
+    /// Tests for the UANodeSet helper.
+    /// </summary>
+    [TestFixture, Category("Server")]
+    [SetCulture("en-us"), SetUICulture("en-us")]
+    [Parallelizable]
+    public class ServerBaseTests : ServerBase
+    {
+        #region Test Setup
+        [OneTimeSetUp]
+        protected void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        protected void OneTimeTearDown()
+        {
+        }
+
+        [SetUp]
+        protected void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+        }
+        #endregion
+
+        #region Test Methods
+        /// <summary>
+        /// Ensure .
+        /// </summary>
+        [Test]
+        [TestCase("opc.tcp://localhost:51210/UA/SampleServer")]
+        public void TranslateEndpointDescriptionsTest(string clientUrl)
+        {
+            Uri clientUri = new Uri(clientUrl);
+            var baseAddresses = new List<BaseAddress>();
+            var endpoints = new List<EndpointDescription>();
+            ApplicationDescription application = new ApplicationDescription();
+
+            var result = this.TranslateEndpointDescriptions(clientUri, baseAddresses, endpoints, application);
+            Assert.NotNull(result);
+        }
+        #endregion
+    }
+}
