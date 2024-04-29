99047e1abb1534ff3023d74649c12899e6434ff1
Martin Regen <mregen@microsoft.com>
2024-04-29 08:37:05 +0200

    copyrights

--- Fuzzing/common/Fuzz.Tools/Program.cs
+++ Fuzzing/common/Fuzz.Tools/Program.cs
@@ -1,12 +1,44 @@
+/* ========================================================================
+ * Copyright (c) 2005-2024 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
 
-using System.IO;
 using System;
-using Mono.Options;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Extensions.Logging;
+using Mono.Options;
 
 public static class Program
 {
+    /// <summary>
+    /// Relative folder references for testcases and findings,
+    /// for when Fuzz.Tools is started from a Visual Studio project.
+    /// </summary>
     public static readonly string RootFolder = "../../../../";
     public static readonly string DefaultTestcasesFolder = RootFolder + "Fuzz/Testcases";
     public static readonly string DefaultFindingsCrashFolder = RootFolder + "findings/crashes/";
