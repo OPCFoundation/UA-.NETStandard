d39e4e83461c8153020d27020873e112e9e03532
ThomasNehring <thomas.nehring@sap.com>
2024-06-28 15:42:05 +0200

    Remove artifacts which were used for debugging.

--- Tests/Opc.Ua.Client.Tests/ClientFixture.cs
+++ Tests/Opc.Ua.Client.Tests/ClientFixture.cs
@@ -352,7 +352,7 @@ namespace Opc.Ua.Client.Tests
         {
             if (m_traceLogger == null)
             {
-                m_traceLogger = NUnitTestLogger<ClientFixture>.Create(writer, Config, TraceMasks);               
+                m_traceLogger = NUnitTestLogger<ClientFixture>.Create(writer, Config, TraceMasks);
             }
             else
             {
