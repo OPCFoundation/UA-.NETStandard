82d675cfd8d6033155efba758955ba87e6b2777b
Roman Ettlinger <romanett98@gmail.com>
2024-11-01 12:16:39 +0100

    fix build

--- Stack/Opc.Ua.Core/Security/Certificates/DirectoryCertificateStore.cs
+++ Stack/Opc.Ua.Core/Security/Certificates/DirectoryCertificateStore.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
