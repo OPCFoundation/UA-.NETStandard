6ad0beb22b766ada0e1d939d6403c0c3620e2977
Roman Ettlinger <romanett98@gmail.com>
2024-12-16 15:39:00 +0100

    fix RejectedCertificateStore

--- Libraries/Opc.Ua.Configuration/ApplicationConfigurationBuilder.cs
+++ Libraries/Opc.Ua.Configuration/ApplicationConfigurationBuilder.cs
@@ -145,7 +145,7 @@ namespace Opc.Ua.Configuration
                     StorePath = DefaultCertificateStorePath(TrustlistType.IssuerUser, pkiRoot)
                 },
                 // rejected store
-                RejectedCertificateStore = new CertificateTrustList() {
+                RejectedCertificateStore = new CertificateStoreIdentifier() {
                     StoreType = rejectedRootType,
                     StorePath = DefaultCertificateStorePath(TrustlistType.Rejected, rejectedRoot)
                 },
