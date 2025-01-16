730e67d992c7b318f2f9951ab7669e8d53ad00d0
Martin Regen <7962757+mregen@users.noreply.github.com>
2025-01-16 20:24:58 +0100

    Update get-version.ps1

--- .azurepipelines/get-version.ps1
+++ .azurepipelines/get-version.ps1
@@ -9,8 +9,8 @@
 
 try {
     # Try install tool
-    # Note: Keep Version 3.6.146, it is known working for 4 digit versioning 
-    & dotnet @("tool", "install", "--tool-path", "./tools", "--version", "3.6.146", "--framework", "net60", "nbgv") 2>&1 
+    # Note: Keep Version 3.7.115, it is known working for 4 digit versioning 
+    & dotnet @("tool", "install", "--tool-path", "./tools", "--version", "3.7.115", "--framework", "net60", "nbgv") 2>&1 
 
     $props = (& ./tools/nbgv  @("get-version", "-f", "json")) | ConvertFrom-Json
     if ($LastExitCode -ne 0) {
