7cbeee71551971d8e3ca1e83699c72335e0a787a
romanett <romanett98@gmail.com>
2025-02-11 13:20:21 +0100

    [Test] Add ECC Certificates to various tests (#2961)
    * Generate ECC Certificates in Client Tests
    
    * Add more tests
    
    * reenable check of certificate
    
    * fix tests
    
    * Dont use random in test
    
    * fix unsupported certificate types
    
    * fix Hash algorithm for nistP384 curve on linux as the friendly name is ECDSA_P384 instead of nistP384
    
    * use friendly name by default and add special case only for linux while taking care of nullability
    
    * disable special casing for finding correct certificates
    
    * fix tests on net 462
    
    * update Certificate Identifier

--- Stack/Opc.Ua.Core/Security/Certificates/CertificateIdentifier.cs
+++ Stack/Opc.Ua.Core/Security/Certificates/CertificateIdentifier.cs
@@ -577,6 +577,10 @@ namespace Opc.Ua
         /// <param name="certificateType">The NodeId of the certificate type.</param>
         public static bool ValidateCertificateType(X509Certificate2 certificate, NodeId certificateType)
         {
+            if (certificateType == null)
+            {
+                return true;
+            }
             switch (certificate.SignatureAlgorithm.Value)
             {
                 case Oids.ECDsaWithSha1:
@@ -594,18 +598,21 @@ namespace Opc.Ua
                     }
 
 
-                    // special cases
-                    if (certType == ObjectTypeIds.EccNistP384ApplicationCertificateType &&
-                        certificateType == ObjectTypeIds.EccNistP256ApplicationCertificateType)
-                    {
-                        return true;
-                    }
+                    // not needed: An end entity Certificate shall use P-256.
+                    // http://opcfoundation.org/UA/SecurityPolicy#ECC_nistP256
+                    //if (certType == ObjectTypeIds.EccNistP384ApplicationCertificateType &&
+                    //    certificateType == ObjectTypeIds.EccNistP256ApplicationCertificateType)
+                    //{
+                    //    return true;
+                    //}
 
-                    if (certType == ObjectTypeIds.EccBrainpoolP384r1ApplicationCertificateType &&
-                        certificateType == ObjectTypeIds.EccBrainpoolP256r1ApplicationCertificateType)
-                    {
-                        return true;
-                    }
+                    // not needed: An end entity Certificate shall use P256r1.
+                    // http://opcfoundation.org/UA/SecurityPolicy#ECC_brainpoolP256r1
+                    //if (certType == ObjectTypeIds.EccBrainpoolP384r1ApplicationCertificateType &&
+                    //    certificateType == ObjectTypeIds.EccBrainpoolP256r1ApplicationCertificateType)
+                    //{
+                    //    return true;
+                    //}
 
                     break;
 
