5406a56138ddb280df4d3fff4adb959a68412591
Roman Ettlinger <romanett98@gmail.com>
2025-03-17 14:57:08 +0100

    fix tests

--- Tests/Opc.Ua.Core.Tests/Stack/Client/ClientTests.cs
+++ Tests/Opc.Ua.Core.Tests/Stack/Client/ClientTests.cs
@@ -100,9 +100,12 @@ namespace Opc.Ua.Core.Tests.Stack.Client
         {
             var appConfig = new ApplicationConfiguration() {
                 ApplicationName = "Test",
-                ClientConfiguration = new ClientConfiguration() {},
+                ClientConfiguration = new ClientConfiguration() { },
                 SecurityConfiguration = new SecurityConfiguration() {
-                    ApplicationCertificate = new CertificateIdentifier()
+                    ApplicationCertificate = new CertificateIdentifier(),
+                    TrustedPeerCertificates = new CertificateTrustList { StorePath = "" },
+                    TrustedIssuerCertificates = new CertificateTrustList { StorePath = "" },
+
                 }
             };
             Assert.DoesNotThrow(() => appConfig.Validate(ApplicationType.Client).GetAwaiter().GetResult());
