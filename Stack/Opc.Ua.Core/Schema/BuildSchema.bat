df7f12f1b0b5ec51dc41928e9181feccae07338a
Randy Armstrong <randy@sparhawksoftware.com>
2025-06-22 20:39:08 -0700

    Fix duplicate pragas.

--- Stack/Opc.Ua.Core/Schema/BuildSchema.bat
+++ Stack/Opc.Ua.Core/Schema/BuildSchema.bat
@@ -1,15 +1,31 @@
 @echo off
 setlocal
 
+set XSD=xsd
+
+where %XSD% >nul 2>&1
+if not %ERRORLEVEL%==0 (
+    echo %XSD% is NOT in the PATH.
+    exit 1
+)
+
+set SVCUTIL=svcutil
+
+where %SVCUTIL% >nul 2>&1
+if not %ERRORLEVEL%==0 (
+    echo %SVCUTIL% is NOT in the PATH.
+    exit 1
+)
+
 echo Processing NodeSet Schema
-xsd /classes /n:Opc.Ua.Export UANodeSet.xsd
+%XSD% /classes /n:Opc.Ua.Export UANodeSet.xsd
 
 echo #pragma warning disable 1591 > temp.txt
 type UANodeSet.cs >> temp.txt
 type temp.txt > UANodeSet.cs
 
 echo Processing SecuredApplication Schema
-svcutil /dconly /namespace:*,Opc.Ua.Security /out:SecuredApplication.cs SecuredApplication.xsd 
+%SVCUTIL% /dconly /namespace:*,Opc.Ua.Security /out:SecuredApplication.cs SecuredApplication.xsd 
 
 echo #pragma warning disable 1591 > temp.txt
 type SecuredApplication.cs >> temp.txt
