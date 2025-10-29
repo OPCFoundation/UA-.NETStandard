b2fbe472eaaca8243fda0138985a75d2b1068274
Roman Ettlinger <romanett98@gmail.com>
2025-10-29 07:52:11 +0100

    Revert "fix SetPublishingMode wrapping SREs in BadUnexpectedError"
    This reverts commit 9f5d1b981212b93bcd6e8ddf8e325e141b55714d.

--- Libraries/Opc.Ua.Server/Subscription/SubscriptionManager.cs
+++ Libraries/Opc.Ua.Server/Subscription/SubscriptionManager.cs
@@ -1250,6 +1250,7 @@ namespace Opc.Ua.Server
                 try
                 {
                     // find subscription.
+
                     if (!m_subscriptions.TryGetValue(subscriptionIds[ii], out ISubscription subscription))
                     {
                         throw new ServiceResultException(StatusCodes.BadSubscriptionIdInvalid);
@@ -1266,7 +1267,7 @@ namespace Opc.Ua.Server
                         diagnosticInfos.Add(null);
                     }
                 }
-                catch (Exception e) when (e is not ServiceResultException)
+                catch (Exception e)
                 {
                     m_logger.LogError(e, "Error occurred in SetPublishingMode");
 
