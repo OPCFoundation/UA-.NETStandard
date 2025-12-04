8a1c1231512d912545c6509fc4548f52279be8ed
Roman Ettlinger <romanett98@gmail.com>
2025-12-04 06:25:22 +0100

    merge update

--- Tests/Opc.Ua.Core.Tests/Stack/State/ConditionStateTests.cs
+++ Tests/Opc.Ua.Core.Tests/Stack/State/ConditionStateTests.cs
@@ -29,6 +29,7 @@
 
 using NUnit.Framework;
 using Opc.Ua.Tests;
+using System;
 
 namespace Opc.Ua.Core.Tests.Stack.State
 {
@@ -62,6 +63,196 @@ namespace Opc.Ua.Core.Tests.Stack.State
         protected void OneTimeTearDown()
         {
             Utils.SilentDispose(m_context);
+        }
+
+        /// <summary>
+        /// Test that SetEnableState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetEnableStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var condition = new ConditionState(null);
+            condition.Create(m_context, new NodeId(1), "Condition", null, true);
+
+            // Set initial state
+            var beforeTime = DateTime.UtcNow;
+            condition.SetEnableState(m_context, true);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated
+            Assert.That(condition.EnabledState.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(condition.EnabledState.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared (all should be None)
+            Assert.That(condition.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+            Assert.That(condition.EnabledState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetSeverity updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetSeverityUpdatesTimestampAndClearsChangeMasks()
+        {
+            var condition = new ConditionState(null);
+            condition.Create(m_context, new NodeId(1), "Condition", null, true);
+
+            var beforeTime = DateTime.UtcNow;
+            condition.SetSeverity(m_context, EventSeverity.High);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamps are updated
+            Assert.That(condition.Severity.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(condition.Severity.Timestamp, Is.LessThanOrEqualTo(afterTime));
+            Assert.That(condition.LastSeverity.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(condition.LastSeverity.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(condition.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetActiveState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetActiveStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var alarm = new AlarmConditionState(m_telemetry, null);
+            alarm.Create(m_context, new NodeId(1), "Alarm", null, true);
+
+            var beforeTime = DateTime.UtcNow;
+            alarm.SetActiveState(m_context, true);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated
+            Assert.That(alarm.ActiveState.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(alarm.ActiveState.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(alarm.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+            Assert.That(alarm.ActiveState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetSuppressedState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetSuppressedStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var alarm = new AlarmConditionState(m_telemetry, null);
+            alarm.Create(m_context, new NodeId(1), "Alarm", null, true);
+            alarm.SuppressedState = new TwoStateVariableState(alarm);
+            alarm.SuppressedState.Create(m_context, null, BrowseNames.SuppressedState, null, false);
+
+            var beforeTime = DateTime.UtcNow;
+            alarm.SetSuppressedState(m_context, true);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated
+            Assert.That(alarm.SuppressedState.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(alarm.SuppressedState.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(alarm.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+            Assert.That(alarm.SuppressedState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetAcknowledgedState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetAcknowledgedStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var condition = new AcknowledgeableConditionState(null);
+            condition.Create(m_context, new NodeId(1), "AckCondition", null, true);
+
+            var beforeTime = DateTime.UtcNow;
+            condition.SetAcknowledgedState(m_context, true);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated
+            Assert.That(condition.AckedState.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(condition.AckedState.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(condition.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+            Assert.That(condition.AckedState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetConfirmedState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetConfirmedStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var condition = new AcknowledgeableConditionState(null);
+            condition.Create(m_context, new NodeId(1), "AckCondition", null, true);
+            condition.ConfirmedState = new TwoStateVariableState(condition);
+            condition.ConfirmedState.Create(m_context, null, BrowseNames.ConfirmedState, null, false);
+
+            var beforeTime = DateTime.UtcNow;
+            condition.SetConfirmedState(m_context, true);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated
+            Assert.That(condition.ConfirmedState.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(condition.ConfirmedState.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(condition.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+            Assert.That(condition.ConfirmedState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that SetShelvingState updates the timestamp and clears change masks.
+        /// </summary>
+        [Test]
+        public void SetShelvingStateUpdatesTimestampAndClearsChangeMasks()
+        {
+            var alarm = new AlarmConditionState(m_telemetry, null);
+            alarm.Create(m_context, new NodeId(1), "Alarm", null, true);
+            alarm.ShelvingState = new ShelvedStateMachineState(alarm);
+            alarm.ShelvingState.Create(m_context, null, BrowseNames.ShelvingState, null, false);
+            alarm.ShelvingState.UnshelveTime = new PropertyState<double>(alarm.ShelvingState);
+
+            var beforeTime = DateTime.UtcNow;
+            alarm.SetShelvingState(m_context, true, false, 1000);
+            var afterTime = DateTime.UtcNow;
+
+            // Verify timestamp is updated on UnshelveTime
+            Assert.That(alarm.ShelvingState.UnshelveTime.Timestamp, Is.GreaterThanOrEqualTo(beforeTime));
+            Assert.That(alarm.ShelvingState.UnshelveTime.Timestamp, Is.LessThanOrEqualTo(afterTime));
+
+            // Verify change masks are cleared
+            Assert.That(alarm.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
+        }
+
+        /// <summary>
+        /// Test that subscribed clients are notified when SetActiveState is called.
+        /// This simulates the issue scenario.
+        /// </summary>
+        [Test]
+        public void SetActiveStateNotifiesSubscribers()
+        {
+            var alarm = new AlarmConditionState(m_telemetry, null);
+            alarm.Create(m_context, new NodeId(1), "Alarm", null, true);
+
+            // Initially inactive
+            alarm.SetActiveState(m_context, false);
+            var initialTimestamp = alarm.ActiveState.Timestamp;
+
+            // Now activate the alarm - timestamp should be greater than or equal to the initial timestamp
+            // since both could be set to DateTime.UtcNow which has limited precision
+            var beforeActivation = DateTime.UtcNow;
+            alarm.SetActiveState(m_context, true);
+            var afterActivation = DateTime.UtcNow;
+
+            // Verify that the timestamp is within the expected range
+            Assert.That(alarm.ActiveState.Timestamp, Is.GreaterThanOrEqualTo(beforeActivation));
+            Assert.That(alarm.ActiveState.Timestamp, Is.LessThanOrEqualTo(afterActivation));
+
+            // Verify that change masks were cleared (indicating subscribers were notified)
+            Assert.That(alarm.ActiveState.ChangeMasks, Is.EqualTo(NodeStateChangeMasks.None));
         }
 
         /// <summary>
