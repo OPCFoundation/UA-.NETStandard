78f2bda9c1c241c93f8e53e8af1da8d305bd5a03
Alexander Köpke <alexander.koepke@microsoft.com>
2026-01-20 15:17:26 +0100

    Add per-namespace NodeSet2 export and CLI option
    Implemented ExportNodesToNodeSet2PerNamespaceAsync in ClientSamples to export nodes into separate NodeSet2 XML files per namespace, excluding OPC Foundation companion specs. Added a helper for safe filename generation. Introduced a new -e/--export command-line option in Program.cs to trigger this export after node fetching. Also added System.Xml.Linq import for potential XML handling.

--- Applications/ConsoleReferenceClient/Program.cs
+++ Applications/ConsoleReferenceClient/Program.cs
@@ -36,10 +36,12 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.Extensions.Logging;
 using Opc.Ua;
 using Opc.Ua.Client;
 using Opc.Ua.Configuration;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Quickstarts.ConsoleReferenceClient
 {
@@ -94,6 +96,7 @@ namespace Quickstarts.ConsoleReferenceClient
             bool leakChannels = false;
             bool forever = false;
             bool enableDurableSubscriptions = false;
+            bool exportNodes = false;
 
             var options = new Mono.Options.OptionSet
             {
@@ -180,6 +183,17 @@ namespace Quickstarts.ConsoleReferenceClient
                         if (b != null)
                         {
                             browseall = true;
+                        }
+                    }
+                },
+                {
+                    "e|export",
+                    "Export all fetched nodes into Nodeset2 xml per default",
+                    e =>
+                    {
+                        if (e != null)
+                        {
+                            exportNodes = true;
                         }
                     }
                 },
@@ -560,6 +574,13 @@ namespace Quickstarts.ConsoleReferenceClient
                                             r.NodeId,
                                             uaClient.Session.NamespaceUris))
                                 ];
+
+                                if (exportNodes)
+                                {
+                                    await samples
+                                        .ExportNodesToNodeSet2PerNamespaceAsync(uaClient.Session, allNodes, Environment.CurrentDirectory)
+                                        .ConfigureAwait(false);
+                                }
                             }
 
                             if (jsonvalues && variableIds != null)
