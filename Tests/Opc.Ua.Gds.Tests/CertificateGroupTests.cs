f77e96ea97fa5e1334403a997a0c09599fc0b448
KircMax <max.kirchberger@siemens.com>
2026-02-10 13:20:43 +0100

    add FindIssuerCAByKeyIdentifierAsync (#3535)
    * add FindIssuerCAByKeyIdentifierAsync
    
    * remove unnecessary using for Org.BouncyCastle.Asn1.X9

--- Tests/Opc.Ua.Gds.Tests/CertificateGroupTests.cs
+++ Tests/Opc.Ua.Gds.Tests/CertificateGroupTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -85,6 +86,59 @@ namespace Opc.Ua.Gds.Tests
                 .FindByThumbprintAsync(certificate.Thumbprint)
                 .ConfigureAwait(false);
             Assert.IsTrue(certs.Count == 1);
+        }
+
+        [Test]
+        public async Task Test_Ca_Signed_Cert_Can_Be_Revoked()
+        {
+            ITelemetryContext telemetry = NUnitTelemetryContext.Create();
+
+            var configuration = new CertificateGroupConfiguration
+            {
+                SubjectName = "CN=GDS Test CA, O=OPC Foundation",
+                BaseStorePath = m_path,
+                CertificateTypes = [nameof(Ua.ObjectTypeIds.RsaSha256ApplicationCertificateType)]
+            };
+            ICertificateGroup certificateGroup = new CertificateGroup(telemetry).Create(
+                m_path + "/authorities",
+                configuration);
+            X509Certificate2 certificate = await certificateGroup
+                .CreateCACertificateAsync(
+                    configuration.SubjectName,
+                    certificateGroup.CertificateTypes[0])
+                .ConfigureAwait(false);
+            Assert.NotNull(certificate);
+            var certificateStoreIdentifier = new CertificateStoreIdentifier(
+                configuration.TrustedListPath);
+            var certificateStoreIdentifier2 = new CertificateStoreIdentifier(
+                configuration.BaseStorePath);
+            ICertificateGroup otherCertGroup = new CertificateGroup(telemetry).Create(
+                m_path + "/authorities",
+                configuration);
+            using var authStore = otherCertGroup.AuthoritiesStore.OpenStore(telemetry);
+            var authStoreCerts = authStore.EnumerateAsync().GetAwaiter().GetResult();    
+
+            var firstAuthStoreCert = authStoreCerts[0];
+            var id = new CertificateIdentifier
+            {
+                Thumbprint = firstAuthStoreCert.Thumbprint,
+                StorePath = authStore.StorePath,
+                StoreType = authStore.StoreType
+            };
+            var authCert = id.LoadPrivateKeyAsync(null).GetAwaiter().GetResult();
+            using ICertificateStore trustedStore = certificateStoreIdentifier.OpenStore(telemetry);
+            var storeCerts = trustedStore.EnumerateAsync().GetAwaiter().GetResult();
+            X509Certificate2Collection certs = await trustedStore
+                .FindByThumbprintAsync(certificate.Thumbprint)
+                .ConfigureAwait(false);
+            Assert.IsTrue(certs.Count >= 1);
+            var signedCert = CertificateBuilder.Create("CN=signedCert")
+               .SetIssuer(authCert)
+               .CreateForRSA();
+            await trustedStore.AddAsync(signedCert).ConfigureAwait(false);
+            var crl = await certificateGroup.RevokeCertificateAsync(signedCert).ConfigureAwait(false);
+            Assert.NotNull(crl);
+            Assert.That(crl.RevokedCertificates.Any(el => el.SerialNumber == signedCert.SerialNumber));
         }
 
         [Test]
