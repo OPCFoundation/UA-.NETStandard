fb3fcc93de49ba8ac18f4a0f423417a328ab45a2
Jonas Jelli <jonas.jelli@de.abb.com>
2026-03-13 09:50:48 +0100

    Add support for certificate based MQTT authentication

---
+++ Tests/Opc.Ua.PubSub.Tests/Transport/MqttPubSubConnectionTests.Mqtts.cs
@@ -0,0 +1,385 @@
+/* ========================================================================
+ * Copyright (c) 2005-2025 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using System.Collections.Generic;
+using System.Threading;
+#if !NET8_0_OR_GREATER
+using MQTTnet.Client;
+#endif
+using NUnit.Framework;
+using Opc.Ua.PubSub.Tests.Encoding;
+using Opc.Ua.PubSub.Transport;
+using Assert = NUnit.Framework.Legacy.ClassicAssert;
+using Opc.Ua.Tests;
+using PubSubEncoding = Opc.Ua.PubSub.Encoding;
+using System.Linq;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using MQTTnet;
+using Opc.Ua.Security.Certificates;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Opc.Ua.PubSub.Tests.Transport
+{
+    [TestFixture(Description = "Tests for Mqtt connections")]
+    public partial class MqttPubSubConnectionTests
+    {
+        internal const string MqttsUrlFormat = $"{Utils.UriSchemeMqtts}://{{0}}:8883";
+
+        [Test]
+        public void ClientCertificateHasPrivateKey()
+        {
+            using X509Certificate2 cert = CertificateBuilder.Create("CN=Subject").CreateForRSA();
+            using TestCertificateDirectory certificateDirectory = new();
+            certificateDirectory.CreateAssets();
+
+            ITelemetryContext telemetry = NUnitTelemetryContext.Create();
+            var mqttTlsCertificates = new MqttTlsCertificates(caCertificatePath: null, certificateDirectory.ClientCertificatePfxPath);
+            var mqttTlsOptions = new MqttTlsOptions(certificates: mqttTlsCertificates);
+
+            var mqttConfiguration = new MqttClientProtocolConfiguration(
+                version: EnumMqttProtocolVersion.V500, mqttTlsOptions: mqttTlsOptions);
+
+            var uaPubSubApplication = UaPubSubApplication.Create(telemetry);
+            var pubSubConnectionDataType = new PubSubConnectionDataType
+            {
+                Address = new ExtensionObject(new NetworkAddressUrlDataType { Url = "mqtts://localhost:8883" }),
+                ConnectionProperties = mqttConfiguration.ConnectionProperties
+            };
+
+            var pubSubConnection = new MqttPubSubConnection(uaPubSubApplication, pubSubConnectionDataType, MessageMapping.Json, telemetry);
+            MqttClientOptions mqttClientOptions = pubSubConnection.PublisherMqttClientOptions;
+            MqttClientTlsOptions channelTlsOptions = mqttClientOptions.ChannelOptions.TlsOptions;
+
+            Assert.IsTrue(channelTlsOptions.UseTls);
+            X509CertificateCollection clientCertificates = channelTlsOptions.ClientCertificatesProvider.GetCertificates();
+            Assert.AreEqual(1, clientCertificates.Count);
+            Assert.IsTrue((clientCertificates[0] as X509Certificate2)!.HasPrivateKey, "Client certificate needs private key");
+        }
+
+#if NET7_0_OR_GREATER
+        [Test(Description = "Validate mqtts local pub/sub connection with json data.")]
+#if !CUSTOM_TESTS
+        [Ignore("A mosquitto tool should be installed local in order to run correctly.")]
+#endif
+        public void ValidateMqttsLocalPubSubConnectionWithJson(
+            [Values((byte)1, (ushort)1, (uint)1, (ulong)1, "abc")] object publisherId,
+            [Values(0, 10000)] double metaDataUpdateTime)
+        {
+            using TestCertificateDirectory certificateDirectory = new();
+            certificateDirectory.CreateAssets();
+
+            ITelemetryContext telemetry = NUnitTelemetryContext.Create();
+            using Process process = RestartMosquitto($"-v -c \"{certificateDirectory.MosquittoConfigFilePath}\"");
+
+            //Arrange
+            const ushort writerGroupId = 1;
+
+            string mqttLocalBrokerUrl = Utils.Format(
+                MqttsUrlFormat,
+                "localhost");
+
+            var mqttTlsCertificates = new MqttTlsCertificates(
+                clientCertificatePath: certificateDirectory.ClientCertificatePfxPath);
+            var mqttTlsOptions = new MqttTlsOptions(certificates: mqttTlsCertificates);
+
+            var mqttConfiguration = new MqttClientProtocolConfiguration(
+                version: EnumMqttProtocolVersion.V500,
+                mqttTlsOptions: mqttTlsOptions);
+
+            const JsonNetworkMessageContentMask jsonNetworkMessageContentMask =
+                JsonNetworkMessageContentMask.NetworkMessageHeader |
+                JsonNetworkMessageContentMask.PublisherId |
+                JsonNetworkMessageContentMask.DataSetMessageHeader;
+            const JsonDataSetMessageContentMask jsonDataSetMessageContentMask
+                = JsonDataSetMessageContentMask.None;
+
+            const DataSetFieldContentMask dataSetFieldContentMask = DataSetFieldContentMask.None;
+
+            var dataSetMetaDataArray = new DataSetMetaDataType[]
+            {
+                MessagesHelper.CreateDataSetMetaData1("DataSet1"),
+                MessagesHelper.CreateDataSetMetaData2("DataSet2"),
+                MessagesHelper.CreateDataSetMetaData3("DataSet3"),
+                MessagesHelper.CreateDataSetMetaDataAllTypes("DataSet4")
+            };
+
+            PubSubConfigurationDataType publisherConfiguration = MessagesHelper
+                .CreatePublisherConfiguration(
+                    Profiles.PubSubMqttJsonTransport,
+                    mqttLocalBrokerUrl,
+                    publisherId: publisherId,
+                    writerGroupId: writerGroupId,
+                    jsonNetworkMessageContentMask: jsonNetworkMessageContentMask,
+                    jsonDataSetMessageContentMask: jsonDataSetMessageContentMask,
+                    dataSetFieldContentMask: dataSetFieldContentMask,
+                    dataSetMetaDataArray: dataSetMetaDataArray,
+                    nameSpaceIndexForData: kNamespaceIndexAllTypes,
+                    metaDataUpdateTime: metaDataUpdateTime);
+            Assert.IsNotNull(publisherConfiguration, "publisherConfiguration should not be null");
+
+            // Configure the mqtt publisher configuration with the MQTTbroker
+            PubSubConnectionDataType mqttPublisherConnection = MessagesHelper.GetConnection(
+                publisherConfiguration,
+                publisherId);
+            Assert.IsNotNull(mqttPublisherConnection, "The MQTT publisher connection is invalid.");
+            mqttPublisherConnection.ConnectionProperties = mqttConfiguration.ConnectionProperties;
+            Assert.IsNotNull(
+                mqttPublisherConnection.ConnectionProperties,
+                "The MQTT publisher connection properties are not valid.");
+
+            // Create publisher application for multiple datasets
+            var publisherApplication = UaPubSubApplication.Create(publisherConfiguration, telemetry);
+            publisherApplication.OnValidateBrokerCertificate = certificateDirectory.ValidateBrokerCertificate;
+            MessagesHelper.LoadData(publisherApplication, kNamespaceIndexAllTypes);
+
+            IUaPubSubConnection publisherConnection = publisherApplication.PubSubConnections[0];
+            Assert.IsNotNull(publisherConnection, "Publisher first connection should not be null");
+
+            Assert.IsNotNull(
+                publisherConfiguration.Connections[0],
+                "publisherConfiguration first connection should not be null");
+            Assert.IsNotNull(
+                publisherConfiguration.Connections[0].WriterGroups[0],
+                "publisherConfiguration  first writer group of first connection should not be null");
+
+            IList<UaNetworkMessage> networkMessages = publisherConnection.CreateNetworkMessages(
+                publisherConfiguration.Connections[0].WriterGroups[0],
+                new WriterGroupPublishState());
+            Assert.IsNotNull(
+                networkMessages,
+                "connection.CreateNetworkMessages shall not return null");
+            Assert.GreaterOrEqual(
+                networkMessages.Count,
+                1,
+                "connection.CreateNetworkMessages shall have at least one network message");
+
+            List<PubSubEncoding.JsonNetworkMessage> uaNetworkMessages = MessagesHelper
+                .GetJsonUaDataNetworkMessages(
+                    [.. networkMessages.Cast<PubSubEncoding.JsonNetworkMessage>()]);
+            Assert.IsNotNull(
+                uaNetworkMessages,
+                "Json ua-data entries are missing from configuration!");
+
+            List<PubSubEncoding.JsonNetworkMessage> uaMetaDataNetworkMessages =
+                MessagesHelper.GetJsonUaMetaDataNetworkMessages(
+                    [.. networkMessages.Cast<PubSubEncoding.JsonNetworkMessage>()]);
+            Assert.IsNotNull(
+                uaMetaDataNetworkMessages,
+                "Json ua-metadata entries are missing from configuration!");
+
+            const bool hasDataSetWriterId =
+                (jsonNetworkMessageContentMask &
+                    JsonNetworkMessageContentMask.DataSetMessageHeader) != 0 &&
+                (jsonDataSetMessageContentMask &
+                    JsonDataSetMessageContentMask.DataSetWriterId) != 0;
+
+            PubSubConfigurationDataType subscriberConfiguration = MessagesHelper
+                .CreateSubscriberConfiguration(
+                    Profiles.PubSubMqttJsonTransport,
+                    mqttLocalBrokerUrl,
+                    publisherId: publisherId,
+                    writerGroupId: writerGroupId,
+                    setDataSetWriterId: hasDataSetWriterId,
+                    jsonNetworkMessageContentMask: jsonNetworkMessageContentMask,
+                    jsonDataSetMessageContentMask: jsonDataSetMessageContentMask,
+                    dataSetFieldContentMask: dataSetFieldContentMask,
+                    dataSetMetaDataArray: dataSetMetaDataArray,
+                    nameSpaceIndexForData: kNamespaceIndexAllTypes);
+            Assert.IsNotNull(subscriberConfiguration, "subscriberConfiguration should not be null");
+
+            // Configure the mqtt subscriber configuration with the MQTTbroker
+            PubSubConnectionDataType mqttSubscriberConnection = MessagesHelper.GetConnection(
+                subscriberConfiguration,
+                publisherId);
+            Assert.IsNotNull(
+                mqttSubscriberConnection,
+                "The MQTT subscriber connection is invalid.");
+            mqttSubscriberConnection.ConnectionProperties = mqttConfiguration.ConnectionProperties;
+            Assert.IsNotNull(
+                mqttSubscriberConnection.ConnectionProperties,
+                "The MQTT subscriber connection properties are not valid.");
+
+            // Create subscriber application for multiple datasets
+            var subscriberApplication = UaPubSubApplication.Create(subscriberConfiguration, telemetry);
+            subscriberApplication.OnValidateBrokerCertificate = certificateDirectory.ValidateBrokerCertificate;
+            Assert.IsNotNull(subscriberApplication, "subscriberApplication should not be null");
+            Assert.IsNotNull(
+                subscriberApplication.PubSubConnections[0],
+                "subscriberConfiguration first connection should not be null");
+
+            List<DataSetReaderDataType> dataSetReaders = subscriberApplication
+                .PubSubConnections[0]
+                .GetOperationalDataSetReaders();
+            Assert.IsNotNull(dataSetReaders, "dataSetReaders should not be null");
+            IUaPubSubConnection subscriberConnection = subscriberApplication.PubSubConnections[0];
+            Assert.IsNotNull(
+                subscriberConnection,
+                "Subscriber first connection should not be null");
+
+            //Act
+            // it will signal if the mqtt message was received from local ip
+            m_uaDataShutdownEvent = new ManualResetEvent(false);
+            // it will signal if the mqtt metadata message was received from local ip
+            m_uaMetaDataShutdownEvent = new ManualResetEvent(false);
+            // it will signal if the changed configuration message was received on local ip
+            m_uaConfigurationUpdateEvent = new ManualResetEvent(false);
+
+            m_isDeltaFrame = false;
+            subscriberApplication.DataReceived += UaPubSubApplication_DataReceived;
+            subscriberApplication.MetaDataReceived += UaPubSubApplication_MetaDataReceived;
+            subscriberApplication.ConfigurationUpdating
+                += UaPubSubApplication_ConfigurationUpdating;
+            subscriberConnection.Start();
+
+            publisherConnection.Start();
+
+            //Assert
+            if (!m_uaDataShutdownEvent.WaitOne(kEstimatedPublishingTime))
+            {
+                NUnit.Framework.Assert.Fail("The JSON message was not received");
+            }
+            if (!m_uaMetaDataShutdownEvent.WaitOne(kEstimatedPublishingTime))
+            {
+                NUnit.Framework.Assert.Fail("The JSON metadata message was not received");
+            }
+
+            subscriberConnection.Stop();
+            publisherConnection.Stop();
+        }
+#endif
+
+
+        /// <summary>
+        /// Creates a temp directory with client and server certificate files and a mqtts mosquitto config.
+        /// Deletes the directory on dispose.
+        /// </summary>
+        private sealed class TestCertificateDirectory : IDisposable
+        {
+            private readonly string m_path;
+            private readonly X509Certificate2 m_clientCert;
+            private readonly X509Certificate2 m_serverCert;
+
+            public TestCertificateDirectory()
+            {
+                m_path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+                m_clientCert = CertificateBuilder.Create("CN=Client").CreateForRSA();
+                m_serverCert = CertificateBuilder.Create("CN=Server").CreateForRSA();
+            }
+
+            public void CreateAssets()
+            {
+                Directory.CreateDirectory(m_path);
+
+                ClientCertificatePfxPath = CombinePath("client.pfx");
+                string clientCertificateDerPath = CombinePath("client.der");
+                string clientCertificateCrtPath = CombinePath("client.crt");
+                string serverCertificateDerPath = CombinePath("server.der");
+                string serverCertificateKeyPath = CombinePath("server.key");
+
+                File.WriteAllBytes(ClientCertificatePfxPath, m_clientCert.Export(X509ContentType.Pfx));
+                File.WriteAllBytes(clientCertificateDerPath, m_clientCert.Export(X509ContentType.Cert));
+#if NET7_0_OR_GREATER
+                string clientCertificatePem = m_clientCert.ExportCertificatePem();
+                File.WriteAllText(clientCertificateCrtPath, clientCertificatePem);
+
+                ServerCertificateCertPath = CombinePath("server.crt");
+
+                string serverCertificatePem = m_serverCert.ExportCertificatePem();
+
+                AsymmetricAlgorithm key = m_serverCert.GetRSAPrivateKey();
+                string privKeyPem = key.ExportPkcs8PrivateKeyPem();
+
+                File.WriteAllText(serverCertificateKeyPath, privKeyPem);
+                File.WriteAllText(ServerCertificateCertPath, serverCertificatePem);
+#endif
+                File.WriteAllBytes(serverCertificateDerPath, m_serverCert.Export(X509ContentType.Cert));
+
+                string mosquittoTlsConfig = CreateMosquittoTlsConfig(clientCertificateCrtPath,
+                    serverCertificateKeyPath, ServerCertificateCertPath);
+                MosquittoConfigFilePath = CombinePath("mosquitto.conf");
+
+                File.WriteAllText(MosquittoConfigFilePath, mosquittoTlsConfig);
+            }
+
+            public string MosquittoConfigFilePath { get; private set; }
+            public string ClientCertificatePfxPath { get; private set; }
+            public string ServerCertificateCertPath { get; private set; }
+
+            private string CombinePath(string fileName)
+            {
+                return Path.Combine(m_path, fileName);
+            }
+
+            private static string CreateMosquittoTlsConfig(string caFile, string keyFile, string certFile)
+            {
+                return new StringBuilder()
+                    .AppendLine("listener 8883")
+                    .Append("cafile ").AppendLine(caFile)
+                    .Append("keyfile ").AppendLine(keyFile)
+                    .Append("certfile ").AppendLine(certFile)
+                    .AppendLine("require_certificate true")
+                    .AppendLine("allow_anonymous true")
+                    .AppendLine("log_type all")
+                    .AppendLine("log_dest stderr")
+                    .AppendLine("connection_messages true")
+                    .ToString();
+            }
+
+            public void Dispose()
+            {
+#pragma warning disable RCS1075 // Avoid empty catch clause that catches System.Exception
+                try
+                {
+                    Directory.Delete(m_path, true);
+                    m_clientCert?.Dispose();
+                    m_serverCert?.Dispose();
+                }
+                catch (Exception)
+                {
+                }
+#pragma warning restore RCS1075 // Avoid empty catch clause that catches System.Exception
+            }
+
+            internal bool ValidateBrokerCertificate(X509Certificate2 brokerCertificate)
+            {
+                return string.Equals(brokerCertificate.Thumbprint, m_serverCert.Thumbprint, StringComparison.OrdinalIgnoreCase);
+            }
+
+            public static implicit operator string(TestCertificateDirectory dir)
+            {
+                return dir?.m_path ?? string.Empty;
+            }
+        }
+    }
+}
