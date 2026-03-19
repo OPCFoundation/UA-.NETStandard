0714b3582bd977f79b0695e90f287b9b1f71520b
mrsuciu <Mircea-Adrian.Suciu@Softing.com>
2026-03-19 18:19:35 +0200

    Adapt TypeInfo tests to master378 API

--- Tests/Opc.Ua.Types.Tests/BuiltIn/TypeInfoTests.cs
+++ Tests/Opc.Ua.Types.Tests/BuiltIn/TypeInfoTests.cs
@@ -19,14 +19,15 @@ namespace Opc.Ua.Types.Tests.BuiltIn
         {
             TypeInfo typeInfo = TypeInfo.Construct(typeof(List<int>));
             Assert.That(typeInfo.BuiltInType, Is.EqualTo(BuiltInType.Int32));
-            Assert.That(typeInfo.IsArray, Is.True);
+            Assert.That(typeInfo.ValueRank, Is.EqualTo(ValueRanks.OneDimension));
         }
 
         [Test]
         public void Construct_ForNonEnumerableGenericType_ReturnsUnknown()
         {
             TypeInfo typeInfo = TypeInfo.Construct(typeof(Task<int>));
-            Assert.That(typeInfo.IsUnknown, Is.True);
+            Assert.That(typeInfo.BuiltInType, Is.EqualTo(BuiltInType.Null));
+            Assert.That(typeInfo.ValueRank, Is.EqualTo(ValueRanks.Any));
         }
     }
 }
