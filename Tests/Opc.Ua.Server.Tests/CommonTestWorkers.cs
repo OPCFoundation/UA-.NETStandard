1b30e5dffca37f8f8a185814eba82f693cd35916
Marc Schier <marcschier@users.noreply.github.com>
2026-03-21 06:21:56 +0100

    Refactor status message assertions in tests

--- Tests/Opc.Ua.Server.Tests/CommonTestWorkers.cs
+++ Tests/Opc.Ua.Server.Tests/CommonTestWorkers.cs
@@ -876,7 +876,8 @@ namespace Opc.Ua.Server.Tests
             Assert.That(publishResponse.NotificationMessage.NotificationData.Count, Is.EqualTo(1));
             string statusMessage = publishResponse.NotificationMessage.NotificationData[0].ToString();
             // Should contain GoodSubscriptionTransferred status code
-            Assert.That(statusMessage, Does.StartWith("StatusChangeNotification\r\nStatus=GoodSubscriptionTransferred"));
+            Assert.That(statusMessage, Does.StartWith("StatusChangeNotification"));
+            Assert.That(statusMessage, Does.Contain("Status=GoodSubscriptionTransferred"));
 
             // static node, do not acknowledge
             Assert.That(publishResponse.AvailableSequenceNumbers.Count, Is.EqualTo(0));
