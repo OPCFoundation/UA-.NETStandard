27bac5593c61c62e2ce3e257dcb116ee4866550e
Marc Schier <marcschier@hotmail.com>
2026-03-28 17:21:46 +0100

    gds

---
+++ Tests/Opc.Ua.Client.AotTests/GdsClientAotTests.cs
@@ -0,0 +1,254 @@
+/* ========================================================================
+ * Copyright (c) 2005-2025 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using Opc.Ua.Gds;
+using Opc.Ua.Gds.Client;
+
+namespace Opc.Ua.Client.AotTests
+{
+    /// <summary>
+    /// AOT integration tests for GDS client operations.
+    /// </summary>
+    [ClassDataSource<GdsTestFixture>(Shared = SharedType.PerTestSession)]
+    public class GdsClientAotTests(GdsTestFixture fixture)
+    {
+        [Test]
+        public async Task ConnectToGds()
+        {
+            GlobalDiscoveryServerClient client = fixture.GdsClient;
+            await Assert.That(client.Session).IsNotNull();
+            await Assert.That(client.Session.Connected).IsTrue();
+        }
+
+        [Test]
+        public async Task RegisterApplication()
+        {
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(
+                "urn:localhost:OPCFoundation:AotTest:Register");
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            await Assert.That(id.IsNull).IsFalse();
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task FindApplication()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:Find";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            ArrayOf<ApplicationRecordDataType> results = await fixture.GdsClient
+                .FindApplicationAsync(appUri)
+                .ConfigureAwait(false);
+
+            await Assert.That(results.IsNull).IsFalse();
+            await Assert.That(results.Count).IsGreaterThanOrEqualTo(1);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task GetApplication()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:Get";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            ApplicationRecordDataType result = await fixture.GdsClient
+                .GetApplicationAsync(id)
+                .ConfigureAwait(false);
+
+            await Assert.That(result).IsNotNull();
+            await Assert.That(result.ApplicationUri).IsEqualTo(appUri);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task UpdateApplication()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:Update";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+            appRecord.ApplicationId = id;
+
+            string updatedUri = appUri + "/v2";
+            appRecord.ApplicationUri = updatedUri;
+            await fixture.GdsClient
+                .UpdateApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            ApplicationRecordDataType result = await fixture.GdsClient
+                .GetApplicationAsync(id)
+                .ConfigureAwait(false);
+
+            await Assert.That(result).IsNotNull();
+            await Assert.That(result.ApplicationUri).IsEqualTo(updatedUri);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task QueryServers()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:QuerySrv";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            ArrayOf<ServerOnNetwork> servers = await fixture.GdsClient
+                .QueryServersAsync(0, null, null, null, default)
+                .ConfigureAwait(false);
+
+            await Assert.That(servers.IsNull).IsFalse();
+            await Assert.That(servers.Count).IsGreaterThanOrEqualTo(1);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task QueryApplications()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:QueryApp";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            (ArrayOf<ApplicationDescription> applications,
+                DateTimeUtc _,
+                uint _) = await fixture.GdsClient
+                .QueryApplicationsAsync(
+                    0, 0,
+                    string.Empty, string.Empty,
+                    0, string.Empty, [])
+                .ConfigureAwait(false);
+
+            await Assert.That(applications.IsNull).IsFalse();
+            await Assert.That(applications.Count).IsGreaterThanOrEqualTo(1);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task GetCertificateGroups()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:CertGroups";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+
+            ArrayOf<NodeId> groups = await fixture.GdsClient
+                .GetCertificateGroupsAsync(id)
+                .ConfigureAwait(false);
+
+            await Assert.That(groups.IsNull).IsFalse();
+            await Assert.That(groups.Count).IsGreaterThanOrEqualTo(1);
+
+            // cleanup
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task UnregisterApplication()
+        {
+            string appUri = "urn:localhost:OPCFoundation:AotTest:Unreg";
+            ApplicationRecordDataType appRecord = CreateTestAppRecord(appUri);
+
+            NodeId id = await fixture.GdsClient
+                .RegisterApplicationAsync(appRecord)
+                .ConfigureAwait(false);
+            await Assert.That(id.IsNull).IsFalse();
+
+            await fixture.GdsClient
+                .UnregisterApplicationAsync(id)
+                .ConfigureAwait(false);
+
+            ArrayOf<ApplicationRecordDataType> results = await fixture.GdsClient
+                .FindApplicationAsync(appUri)
+                .ConfigureAwait(false);
+
+            await Assert.That(results.Count).IsEqualTo(0);
+        }
+
+        private static ApplicationRecordDataType CreateTestAppRecord(
+            string applicationUri)
+        {
+            return new ApplicationRecordDataType
+            {
+                ApplicationUri = applicationUri,
+                ApplicationType = ApplicationType.Server,
+                ApplicationNames =
+                    [new LocalizedText("en-US", "AOT Test Application")],
+                ProductUri = "http://opcfoundation.org/UA/AotTest",
+                DiscoveryUrls = [$"opc.tcp://localhost:4840/{applicationUri}"],
+                ServerCapabilities = ["NA"]
+            };
+        }
+    }
+}
---
+++ Tests/Opc.Ua.Client.AotTests/GdsTestFixture.cs
@@ -0,0 +1,365 @@
+/* ========================================================================
+ * Copyright (c) 2005-2025 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Opc.Ua.Configuration;
+using Opc.Ua.Gds.Client;
+using Opc.Ua.Gds.Server;
+using Opc.Ua.Gds.Server.Database;
+using Opc.Ua.Gds.Server.Database.Linq;
+using Opc.Ua.Server;
+using Opc.Ua.Server.Tests;
+using Opc.Ua.Server.UserDatabase;
+using TUnit.Core.Interfaces;
+
+namespace Opc.Ua.Client.AotTests
+{
+    /// <summary>
+    /// Shared fixture that starts a GDS server in-process and provides
+    /// a connected <see cref="GlobalDiscoveryServerClient"/> for tests.
+    /// Builds the server configuration programmatically to avoid
+    /// config-file dependencies in the AOT test environment.
+    /// </summary>
+    public sealed class GdsTestFixture : IAsyncInitializer, IAsyncDisposable
+    {
+        public GlobalDiscoverySampleServer Server { get; private set; }
+        public GlobalDiscoveryServerClient GdsClient { get; private set; }
+        public ITelemetryContext Telemetry { get; private set; }
+        public string EndpointUrl { get; private set; }
+        public int BasePort { get; private set; }
+
+        private ApplicationInstance m_serverApplication;
+        private ApplicationConfiguration m_clientConfiguration;
+        private string m_gdsRoot;
+        private string m_pkiRoot;
+
+        public async Task InitializeAsync()
+        {
+            Telemetry = DefaultTelemetry.Create(builder =>
+                builder.SetMinimumLevel(LogLevel.Warning));
+
+            m_gdsRoot = Path.Combine(Path.GetTempPath(), "OPC", "AotGDS");
+            m_pkiRoot = Path.Combine(
+                Path.GetTempPath(), "OpcUaAotGdsTests", "pki");
+
+            // Clean any previous state
+            CleanDirectory(m_gdsRoot);
+
+            // Start GDS server with retry logic
+            int testPort = ServerFixtureUtils.GetNextFreeIPPort();
+            bool retryStartServer;
+            int serverStartRetries = 25;
+            do
+            {
+                retryStartServer = false;
+                try
+                {
+                    await StartGdsServerAsync(testPort).ConfigureAwait(false);
+                }
+                catch (ServiceResultException sre)
+                {
+                    serverStartRetries--;
+                    if (Server != null)
+                    {
+                        await Server.StopAsync().ConfigureAwait(false);
+                        Server.Dispose();
+                        Server = null;
+                    }
+                    testPort = UnsecureRandom.Shared.Next(
+                        ServerFixtureUtils.MinTestPort,
+                        ServerFixtureUtils.MaxTestPort);
+                    if (serverStartRetries == 0 ||
+                        sre.StatusCode != StatusCodes.BadNoCommunication)
+                    {
+                        throw;
+                    }
+                    retryStartServer = true;
+                }
+                await Task.Delay(UnsecureRandom.Shared.Next(100, 1000))
+                    .ConfigureAwait(false);
+            } while (retryStartServer);
+
+            BasePort = testPort;
+            EndpointUrl =
+                $"opc.tcp://localhost:{BasePort}/GlobalDiscoveryTestServer";
+
+            // Build a client configuration programmatically
+            m_clientConfiguration = new ApplicationConfiguration(Telemetry)
+            {
+                ApplicationName = "AotGdsTestClient",
+                ApplicationUri =
+                    "urn:localhost:OPCFoundation:AotGdsTestClient",
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier
+                    {
+                        StoreType = CertificateStoreType.Directory,
+                        StorePath = Path.Combine(m_pkiRoot, "own"),
+                        SubjectName =
+                            "CN=AotGdsTestClient, O=OPC Foundation"
+                    },
+                    TrustedIssuerCertificates = new CertificateTrustList
+                    {
+                        StoreType = CertificateStoreType.Directory,
+                        StorePath = Path.Combine(m_pkiRoot, "issuer")
+                    },
+                    TrustedPeerCertificates = new CertificateTrustList
+                    {
+                        StoreType = CertificateStoreType.Directory,
+                        StorePath = Path.Combine(m_pkiRoot, "trusted")
+                    },
+                    RejectedCertificateStore = new CertificateTrustList
+                    {
+                        StoreType = CertificateStoreType.Directory,
+                        StorePath = Path.Combine(m_pkiRoot, "rejected")
+                    },
+                    AutoAcceptUntrustedCertificates = true
+                },
+                TransportQuotas = new TransportQuotas(),
+                ClientConfiguration = new ClientConfiguration(),
+                ServerConfiguration = new ServerConfiguration()
+            };
+            await m_clientConfiguration.ValidateAsync(ApplicationType.Client)
+                .ConfigureAwait(false);
+            m_clientConfiguration.CertificateValidator
+                .CertificateValidation += (s, e) => e.Accept = true;
+
+            // Create the GDS client with admin credentials
+            GdsClient = new GlobalDiscoveryServerClient(
+                m_clientConfiguration);
+
+            // Select the None security endpoint for simplicity
+            var endpointConfiguration =
+                EndpointConfiguration.Create(m_clientConfiguration);
+            using var discoveryClient = await DiscoveryClient.CreateAsync(
+                new Uri(EndpointUrl),
+                endpointConfiguration,
+                Telemetry).ConfigureAwait(false);
+            ArrayOf<EndpointDescription> endpoints =
+                await discoveryClient.GetEndpointsAsync(
+                    default, CancellationToken.None)
+                    .ConfigureAwait(false);
+            await discoveryClient.CloseAsync(CancellationToken.None)
+                .ConfigureAwait(false);
+
+            EndpointDescription selectedEndpoint = null;
+            foreach (EndpointDescription ep in endpoints)
+            {
+                if (ep.SecurityPolicyUri == SecurityPolicies.None)
+                {
+                    selectedEndpoint = ep;
+                    break;
+                }
+            }
+            selectedEndpoint ??= endpoints[0];
+
+            GdsClient.Endpoint = new ConfiguredEndpoint(
+                null, selectedEndpoint, endpointConfiguration);
+            GdsClient.AdminCredentials = new UserIdentity(
+                "appadmin", Encoding.UTF8.GetBytes("demo"));
+            await GdsClient.ConnectAsync(CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (GdsClient != null)
+            {
+                try
+                {
+                    await GdsClient.DisconnectAsync().ConfigureAwait(false);
+                }
+                catch
+                {
+                    // ignore disconnect errors during cleanup
+                }
+                GdsClient.Dispose();
+                GdsClient = null;
+            }
+
+            if (Server != null)
+            {
+                using GlobalDiscoverySampleServer server = Server;
+                Server = null;
+                await server.StopAsync().ConfigureAwait(false);
+            }
+
+            CleanDirectory(m_pkiRoot);
+            CleanDirectory(m_gdsRoot);
+        }
+
+        [UnconditionalSuppressMessage("AOT",
+            "IL2026:RequiresUnreferencedCode",
+            Justification = "Test-only code; GDS config serialization " +
+            "is exercised at runtime.")]
+        [UnconditionalSuppressMessage("AOT",
+            "IL3050:RequiresDynamicCode",
+            Justification = "Test-only code; GDS config serialization " +
+            "is exercised at runtime.")]
+        private async Task StartGdsServerAsync(int port)
+        {
+            string[] baseAddresses =
+                [$"opc.tcp://localhost:{port}/GlobalDiscoveryTestServer"];
+
+            var gdsConfig = new GlobalDiscoveryServerConfiguration
+            {
+                AuthoritiesStorePath =
+                    Path.Combine(m_gdsRoot, "authorities"),
+                ApplicationCertificatesStorePath =
+                    Path.Combine(m_gdsRoot, "applications"),
+                DefaultSubjectNameContext = "O=OPC Foundation",
+                CertificateGroups =
+                [
+                    new CertificateGroupConfiguration
+                    {
+                        Id = "Default",
+                        CertificateTypes =
+                        [
+                            "RsaSha256ApplicationCertificateType"
+                        ],
+                        SubjectName =
+                            "CN=GDS Test CA, O=OPC Foundation",
+                        BaseStorePath =
+                            Path.Combine(m_gdsRoot, "CA", "default"),
+                        DefaultCertificateHashSize = 256,
+                        DefaultCertificateKeySize = 2048,
+                        DefaultCertificateLifetime = 12,
+                        CACertificateHashSize = 512,
+                        CACertificateKeySize = 4096,
+                        CACertificateLifetime = 60
+                    }
+                ],
+                DatabaseStorePath =
+                    Path.Combine(m_gdsRoot, "gdsdb.json"),
+                UsersDatabaseStorePath =
+                    Path.Combine(m_gdsRoot, "gdsusersdb.json")
+            };
+
+            CertificateIdentifierCollection applicationCerts =
+                ApplicationConfigurationBuilder
+                    .CreateDefaultApplicationCertificates(
+                        "CN=GDS AOT Test Server, O=OPC Foundation, " +
+                        "DC=localhost",
+                        CertificateStoreType.Directory,
+                        m_gdsRoot);
+
+            m_serverApplication = new ApplicationInstance(Telemetry)
+            {
+                ApplicationName = "GDS AOT Test Server",
+                ApplicationType = ApplicationType.Server,
+                ConfigSectionName = "Opc.Ua.GdsAotTestServer"
+            };
+
+            ApplicationConfiguration config = await m_serverApplication
+                .Build(
+                    "urn:localhost:opcfoundation.org:GdsAotTestServer",
+                    "http://opcfoundation.org/UA/GdsAotTestServer")
+                .AsServer(baseAddresses)
+                .AddUnsecurePolicyNone()
+                .AddSignAndEncryptPolicies()
+                .AddUserTokenPolicy(UserTokenType.Anonymous)
+                .AddUserTokenPolicy(UserTokenType.UserName)
+                .SetDiagnosticsEnabled(true)
+                .AddServerCapabilities("GDS")
+                .AddServerProfile(
+                    "http://opcfoundation.org/UA-Profile/Server/" +
+                    "GlobalDiscoveryAndCertificateManagement2017")
+                .SetShutdownDelay(0)
+                .AddSecurityConfiguration(applicationCerts, m_gdsRoot)
+                .SetAutoAcceptUntrustedCertificates(true)
+                .SetRejectSHA1SignedCertificates(false)
+                .SetRejectUnknownRevocationStatus(true)
+                .SetMinimumCertificateKeySize(1024)
+                .AddExtension<GlobalDiscoveryServerConfiguration>(
+                    null, gdsConfig)
+                .SetDeleteOnLoad(true)
+                .CreateAsync()
+                .ConfigureAwait(false);
+
+            bool haveAppCertificate = await m_serverApplication
+                .CheckApplicationInstanceCertificatesAsync(true)
+                .ConfigureAwait(false);
+            if (!haveAppCertificate)
+            {
+                throw new InvalidOperationException(
+                    "Application instance certificate invalid!");
+            }
+
+            // Load databases and register users
+            var applicationsDatabase = JsonApplicationsDatabase.Load(
+                gdsConfig.DatabaseStorePath);
+            IUserDatabase userDatabase = JsonUserDatabase.Load(
+                gdsConfig.UsersDatabaseStorePath, Telemetry);
+
+            userDatabase.CreateUser("sysadmin", "demo"u8,
+                [GdsRole.CertificateAuthorityAdmin,
+                 GdsRole.DiscoveryAdmin,
+                 Role.SecurityAdmin, Role.ConfigureAdmin]);
+            userDatabase.CreateUser("appadmin", "demo"u8,
+                [Role.AuthenticatedUser,
+                 GdsRole.CertificateAuthorityAdmin,
+                 GdsRole.DiscoveryAdmin]);
+            userDatabase.CreateUser("appuser", "demo"u8,
+                [Role.AuthenticatedUser]);
+
+            Server = new GlobalDiscoverySampleServer(
+                applicationsDatabase,
+                applicationsDatabase,
+                new CertificateGroup(Telemetry),
+                userDatabase,
+                Telemetry);
+            await m_serverApplication.StartAsync(Server)
+                .ConfigureAwait(false);
+
+            if (Server.CurrentState != ServerState.Running)
+            {
+                throw new ServiceResultException(
+                    "GDS server failed to start");
+            }
+        }
+
+        private static void CleanDirectory(string path)
+        {
+            if (path != null && Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, true);
+                }
+                catch
+                {
+                    // ignore cleanup errors
+                }
+            }
+        }
+    }
+}
