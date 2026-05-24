aa05bc294314fe53d09512b0b072a2c9f52d2eea
JMolenkamp <joostmolenkamp@gmail.com>
2026-05-24 11:06:18 +0200

    fix(type-info): prioritize encodeable check over enumerable check

--- Stack/Opc.Ua.Types/BuiltIn/TypeInfo.cs
+++ Stack/Opc.Ua.Types/BuiltIn/TypeInfo.cs
@@ -1650,6 +1650,13 @@ namespace Opc.Ua
                     return Unknown;
                 }
 
+                // check for encodeable object.
+                if (typeof(IEncodeable).GetTypeInfo().IsAssignableFrom(systemType.GetTypeInfo()) ||
+                    name == "IEncodeable")
+                {
+                    return Scalars.ExtensionObject;
+                }
+
                 // check for generic type.
                 if (systemType.GetTypeInfo().IsGenericType)
                 {
@@ -1674,13 +1681,6 @@ namespace Opc.Ua
                     }
 
                     return Unknown;
-                }
-
-                // check for encodeable object.
-                if (typeof(IEncodeable).GetTypeInfo().IsAssignableFrom(systemType.GetTypeInfo()) ||
-                    name == "IEncodeable")
-                {
-                    return Scalars.ExtensionObject;
                 }
 
                 return Unknown;
